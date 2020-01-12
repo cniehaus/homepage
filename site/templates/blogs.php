5bd65a97f929ddc99157b1ac0b0f203e574af06e
Carsten Niehaus <gitlab@niehaus-ol.de>
2020-01-12 11:39:30 +0100

    Blogsübersicht

---
+++ site/templates/blogs.php
@@ -0,0 +1,17 @@
+<?php snippet('header') ?>
+
+
+<ul class="projects">
+
+    <?php foreach ($page->children()->listed() as $project): ?>
+        <li>
+            <a href="<?= $project->url() ?>">
+                <?= $project->title() ?>
+            </a>
+        </li>
+    <?php endforeach ?>
+
+</ul>
+
+
+<?php snippet('footer') ?>
