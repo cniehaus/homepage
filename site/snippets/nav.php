175c00d4e6317c79097319666545c21e7cc00c1e
Carsten Niehaus <gitlab@niehaus-ol.de>
2020-01-14 22:52:45 +0100

    Anpassungen an's Menu

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,9 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-light">
-
-
-
+<nav class="navbar navbar-expand-sm navbar-fixed-top navbar-light bg-light">
   <a class="navbar-brand" href="<?= $site->url() ?>">KGS Rastede</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -15,13 +12,17 @@
         <a class="nav-link" href="<?= page('kontakte')->url() ?>">Kontakt</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="<?= page('Faecher')->url() ?>" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          aus den Fachbereichen
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="<?= page('blogs')->children()->listed()->filterBy('tags', 'NTW', ',')  ?>">NTW</a>
-          <a class="dropdown-item" href="#">Wirtschaft</a>
+      <a class="nav-link dropdown-toggle" 
+          href="#" id="navbarDropdown" 
+          role="button" 
+          data-toggle="dropdown">Fachbereiche</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Arbeit/Wirtschaft</a>
           <a class="dropdown-item" href="#">Sport</a>
+          <a class="dropdown-item" href="#">Mukubi</a>
+          <a class="dropdown-item" href="#">Fremdsprachen</a>
+          <a class="dropdown-item" href="#">Deutsch</a>
+          <a class="dropdown-item" href="#">Mathematik</a>
         </div>
       </li>
     </ul>
