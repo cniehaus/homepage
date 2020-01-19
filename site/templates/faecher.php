c16425dc9d574051ee86bc2857d6bf02c6a66938
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-19 22:15:54 +0100

    daecher.php in der ersten Version

--- site/templates/faecher.php
+++ site/templates/faecher.php
@@ -1,11 +1,13 @@
 <?php snippet('header') ?>
 
 
+<h1>Über die Fächer</h1>
+
 <?php snippet('blogs', [
   'blogs' => page('blogs')
     ->children()
     ->listed()
-    ->filterBy('tags', 'berlin', ',')
+    ->filterBy('tags', 'Biologie', ',')
   ]) ?>
 
 <?php snippet('footer') ?>
