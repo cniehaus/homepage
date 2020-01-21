042620b3fbfaca618ce7d6d824d953a4e4adfac2
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-21 15:29:07 +0100

    Menü zeigt auf korrekte Kontakte

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -7,13 +7,19 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">&Uuml;ber die Schule</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= page('kontakte')->url() ?>">Kontakt</a>
-        </li>
+      <ul class="navbar-nav">       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Über die Schule</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Anfahrt</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Arbeit/Wirtschaft</a>
+            <a class="dropdown-item" href="<?= page('kontakte/hausmeister')->url() ?>">Hausmeister</a>
+            <a class="dropdown-item" href="<?= page('kontakte/schulleitung')->url() ?>">Schuleitung</a>
+            <a class="dropdown-item" href="<?= page('kontakte/fbl')->url() ?>">Fachbereichsleiter</a>
+            <a class="dropdown-item" href="<?= page('kontakte/sekretariate')->url() ?>">Sekretariate</a>
+          </div>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Fachbereiche</a>
           <div class="dropdown-menu">
