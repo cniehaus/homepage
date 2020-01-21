fd661017e6bf39e12d777048d9a403493aa74edf
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-21 19:28:31 +0100

    deutlich hübscheres CSS

--- site/templates/kontakte.php
+++ site/templates/kontakte.php
@@ -3,18 +3,25 @@
 <h1><?= $page->title() ?></h1>
 
 
+<div class="jumbotron">
+  <h1 class="display-4"><?= $page->title() ?></h1>
+  <p class="lead"><?= $page->text() ?></p>
+</div>
+
+
 <div class="row row-cols-sm-2 row-cols-md-5">
     <?php foreach ($page->children() as $kontakt) : ?>
         <div class="card">
             <img src="<?= $kontakt->images()->first()->url() ?>" class="rounded card-img-top" alt="<?= $kontakt->images()->first()->alt() ?>">
-            <div class="card-body">
-                <h3 class="card-title"><?= $kontakt->position() ?></h3>
-                <p class="card-text p-3 mb-2"><?= $kontakt->title() ?></p>
+            <div class="card-body bg-dark text-white">
+                <h2 class="card-title"><?= $kontakt->position() ?></h2>
+                <h3 class="card-subtitle"><?= $kontakt->title() ?></h3>
+                <hr class="my-4">
+                <p class="card-text p-3 mb-2">&#9993;<?= $kontakt->email() ?></p>
+                <p class="card-text p-3 mb-2">&#9743; <?= $kontakt->phone() ?></p>
             </div>
-            <ul class="list-group list-group-flush  p-3 mb-2">
-                <li class="list-group-item"><?= $kontakt->email() ?></li>
-                <li class="list-group-item"><?= $kontakt->phone() ?></li>
-            </ul>
+            
+                
         </div>
     <?php endforeach ?>
 </div>
