52619cee97752d7faef5a5ff350faa8193936382
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-23 22:10:27 +0100

    Bild neben dem Blogartikel

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,18 +1,31 @@
 <?php snippet('header') ?>
 
+<h1>Aktuelles aus der KGS</h1>
+
 <?php foreach (page('blogs')->children()->filterBy('featured', true) as $subpage) : ?>
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $subpage->title() ?></h5>
-                    <p class="card-text"><?= $subpage->Text()->excerpt(150) ?></p>
-                    <a href="<?= $subpage->url() ?>" class="btn btn-primary">...weiterlesen</a>
-                </div>
-            </div>
-        </div>
+
+<div class="card mb-2">
+
+  <div class="row no-gutters">
+    <div class="col-md-2">
+        <?php if( $subpage->hasImages() > 0 ) : ?>
+             <img src="<?= $subpage->images()->first()->url() ?>" class="card-img" alt="..."> 
+        <?php else: ?>
+          <h5>kein bild</h5>
+        <?php endif ?> 
     </div>
+    <div class="col-md-2">
+      <div class="card-body">
+        <h3 class="card-title"><?= $subpage->title() ?></h3>
+        <p class="card-text"><?= $subpage->Text()->blocks()->excerpt(150) ?></p>
+        <a href="<?= $subpage->url() ?>" class="btn btn-primary">...weiterlesen</a>
+
+      </div>
+    </div>
+  </div>
+</div>
 <?php endforeach ?>
+
 
 
 <?php snippet('footer') ?>
