32350cf59000d4fa7e548d62c324a2d429979b7a
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-26 19:28:05 +0100

    Menu mit mehr Fächern

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -35,20 +35,42 @@
             <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
               <i class="material-icons">subject</i> &Uuml;ber die F&auml;cher
             </a>
-            <div class="dropdown-menu dropdown-with-icons">     
+            <div class="dropdown-menu dropdown-with-icons"> 
+            <!-- Sport -->    
             <a class="dropdown-item" href="<?= page('Faecher/Sport')->url() ?>"><i class="material-icons">sports_football</i>Sport</a>
+            <div class="dropdown-divider"></div>
+
+            <!-- NTW -->
             <a class="dropdown-item" href="<?= page('Faecher/Physik')->url() ?>"><i class="material-icons">battery_charging_full</i>Physik</a>
             <a class="dropdown-item" href="<?= page('Faecher/Biologie')->url() ?>"><i class="material-icons">filter_vintage</i>Biologie</a>
             <a class="dropdown-item" href="<?= page('Faecher/Chemie')->url() ?>"><i class="material-icons">outdoor_grill</i>Chemie</a>
+            <div class="dropdown-divider"></div>
+            
+            <!-- AWT / Informatik -->
             <a class="dropdown-item" href="<?= page('Faecher/Informatik')->url() ?>"><i class="material-icons">computer</i>Informatik</a>
-            <a class="dropdown-item" href="<?= page('Faecher/Mathematik')->url() ?>"><i class="material-icons">add</i>Mathematik</a>
+            <a class="dropdown-item" href="<?= page('Faecher/technik')->url() ?>"><i class="material-icons">traffic</i>Technik</a>
+            <a class="dropdown-item" href="<?= page('Faecher/hauswirtschaft')->url() ?>"><i class="material-icons">emoji_food_beverage</i>Hauswirtschaft</a>
+            <div class="dropdown-divider"></div>
+
+            <!-- GSW -->
+            <a class="dropdown-item" href="<?= page('Faecher/politik-wirtschaft')->url() ?>"><i class="material-icons">monetization_on</i>Politik/Wirtschaft</a>
+            <a class="dropdown-item" href="<?= page('Faecher/geschichte')->url() ?>"><i class="material-icons">history</i>Geschichte</a>
+            <a class="dropdown-item" href="<?= page('Faecher/erdkunde')->url() ?>"><i class="material-icons">landscape</i>Erdkunde</a>
+            <a class="dropdown-item" href="<?= page('Faecher/religion')->url() ?>"><i class="material-icons">thumbs_up_down</i>Religion</a>
             <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">thumbs_up_down</i>Werte und Normen</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">thumbs_up_down</i>Religion</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">traffic</i>Technik</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">emoji_food_beverage</i>Hauswirtschaft</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">history</i>Geschichte</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">landscape</i>Erdkunde</a>
-            <a class="dropdown-item" href="<?= page('Faecher/WuN')->url() ?>"><i class="material-icons">monetization_on</i>Politik/Wirtschaft</a>
+            <div class="dropdown-divider"></div>
+
+            <!-- Mukubi -->
+            <a class="dropdown-item" href="<?= page('Faecher/musik')->url() ?>"><i class="material-icons">music_note</i>Musik</a>
+            <a class="dropdown-item" href="<?= page('Faecher/kunst')->url() ?>"><i class="material-icons">photo</i>Kunst</a>
+            <div class="dropdown-divider"></div>
+
+            <!-- Mathematik -->
+            <a class="dropdown-item" href="<?= page('Faecher/Mathematik')->url() ?>"><i class="material-icons">add</i>Mathematik</a>
+
+            
+            
+
             </div>
           </li>
         </ul>
