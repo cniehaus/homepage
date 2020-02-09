31f8577e6f3949efe54b1e2d05b287d7e8fd24b4
Carsten Niehaus <ni@kgs-rastede.de>
2020-02-09 12:04:50 +0100

    Überflüssige Zeile kann weg

--- site/templates/krankmeldung.php
+++ site/templates/krankmeldung.php
@@ -27,22 +27,6 @@
       </select>
     </div>
   </div>
-
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">Klassenlehrer*in</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputState">Klasse</label>
-      <select id="inputState" class="form-control">
-        <option selected>05a1</option>
-        <option>05b1</option>
-        <option>05a2</option>
-        <option>05a3</option>
-      </select>
-    </div>
-  </div>
   <div class="form-group">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck">
