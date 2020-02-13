c06b611da1024ac77bf1f7a623fc12e507836005
Carsten Niehaus <ni@kgs-rastede.de>
2020-02-13 20:26:13 +0100

    Toter Code

--- site/templates/lageplan.php
+++ site/templates/lageplan.php
@@ -5,20 +5,6 @@
 
 <?= $page->text() ?>
 
-<ul class="projects">
-
-    <?php foreach ($page->children()->listed() as $project): ?>
-        <li>
-            <a href="<?= $project->url() ?>">
-                <?= $project->title() ?>
-            </a>
-        </li>
-    <?php endforeach ?>
-
-</ul>
-
-
-
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
 
 <!-- Make sure you put this AFTER Leaflet's CSS -->
