bf64289cd92a183acf28fca2e71e61468e5691af
Carsten Niehaus <ni@kgs-rastede.de>
2020-02-24 14:36:09 +0100

    Die Defaultseite sollte ein 9/3 Layouthaben für die zweite Spalte für Links

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,25 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-<?= $page->text()->kirbytext() ?>
-    
+
+<h1>default.php</h1>
+
+<div class="container">
+    <div class="content">
+        <div class="row">
+            <div class="col-md-9">
+                <?= $page->text()->kirbytext() ?>
+            </div>
+            <div class="col-md-3">
+                <p>Moin moin </p>
+                <!-- Hier steht nix -->
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
 <?php snippet('footer') ?>
