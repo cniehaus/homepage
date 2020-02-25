1499cb95f25b34d2a9a39175d534b3b03aac1e5d
Fynn Kunzmann <max@mustermann.de>
2020-02-25 21:50:16 +0100

    eig richtig, aber Fehlermeldung am Ende

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,42 +1,27 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-
 <div class="container">
     <div class="content">
-    <?php if ($page->filterBy('toggle', true))
-        {
-            ?>
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="container-md-flex"><?= $page->text()->kirbytext() ?></div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card"><a href="#"><?= $page->name() ?></a></div>
-                    </div>
+        <?php if($page->toggle()->bool() === true) :
+            {?>
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="container"><?= $page->text()->kirbytext() ?></div>
                 </div>
-        <?php
-        }
-        else
-        {
-            ?>
-                dfdfs
-            <?php
-        }
-        ?>
+                <div class="col-md-3">
+                    <div class="card"><a href="#"><?= $page->name() ?></a></div>
+                </div>
+            </div>
+            <?php }
+            else:
+            {?>
+                sdfjkdsfh
+
+            }
+
     </div>
 </div>
-
-
-<?php
-if (true)
-{
-    ?><? ?><?php
-}
-else?>
-{
-
-}
 
 
 <?php snippet('footer') ?>
