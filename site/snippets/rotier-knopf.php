a895dd4a84f1248b114c7ed82ad232adc94fcaf8
Fynn Kunzmann <max@mustermann.de>
2020-03-01 14:40:50 +0100

    funktioniert, nur Datum fehlt noch

--- site/snippets/rotier-knopf.php
+++ site/snippets/rotier-knopf.php
@@ -1,8 +1,8 @@
-<div class="col-md-6">
+<div class="col-md-6 ml-auto mr-auto">
     <div class="card bg-success text-center">
         <div class="card-body">
             <h3 class="card-title"><?= $subpage->titel() ?></h3>
-            <p class="card-description"><?= $subpage->textvorne() ?></p>
+            <p class="card-description"><?= $subpage->text() ?></p>
             <?php if($image = $subpage->datei()->toFile()): ?>
                 <a href="<?php $subpage->datei()->toFile() ?>" download="18.png" class="btn btn-warning btn-round">Download</a>
             <?php endif ?>
