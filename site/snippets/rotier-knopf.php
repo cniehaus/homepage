ca24c17e681533d3fd21335290b5dcca888a5b0e
Fynn Kunzmann <max@mustermann.de>
2020-03-01 18:51:59 +0100

    Icons hinzugefügt

--- site/snippets/rotier-knopf.php
+++ site/snippets/rotier-knopf.php
@@ -4,10 +4,10 @@
             <h3 class="card-title"><?= $subpage->titel() ?></h3>
             <p class="card-description"><?= $subpage->text() ?></p>
             <?php if($file = $subpage->datei()->toFile()): ?>
-                <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-warning btn-round"><?php echo $file->name() ?></a>
+                <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-warning btn-round"><i class="material-icons">get_app</i><?php echo $file->name() ?></a>
             <?php endif ?>
             <?php if($subpage->link() != ""): ?>
-                <a href="<?= $subpage->link() ?>" class="btn btn-warning btn-round" role="button">Mehr zum Thema</a>
+                <a href="<?= $subpage->link() ?>" class="btn btn-warning btn-round" role="button"><i class="material-icons">info</i>Mehr zum Thema</a>
             <?php endif ?>
         </div>
     </div>
