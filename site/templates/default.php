4663772e35aa809169b76153d47f883fe5a46a99
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-17 09:16:50 +0100

    Sidebar als Snippet bereitstellen

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,33 +2,7 @@
 <?php snippet('page-header') ?>
 
 
-<div class="container">
-    <div class="content">
-        <?php if($page->toggle()->bool() === true) : ?>
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="container"><?= $page->text()->blocks() ?></div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-header card-header-info">
-                            <h5 class="card-title"><?= $page->sidetitel()->kirbytext() ?></h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="list-group">
-                                <?php foreach ($page->sidebar()->toStructure() as $sidebar): ?>
-                                    <a href="<?= $sidebar->link() ?>" class="list-group-item list-group-item-action"><?= $sidebar->name() ?></a></li>
-                                <?php endforeach ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php else: ?>
-            <div class="container"><?= $page->text()->blocks() ?></div>
-        <?php endif ?>
-    </div>
-</div>
+<?php snippet('sidebar') ?>
 
 
 <?php snippet('footer') ?>
