da4c7f362960be7069364a2ccf86d1ae7b17bdb5
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-19 11:39:10 +0100

    wir nutzen einfach google calender...

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -8,136 +8,51 @@
     margin: 0 auto;
   }
 </style>
+<script src='https://unpkg.com/@fullcalendar/daygrid@4.4.0/main.min.js'></script>
+
+<script src='https://unpkg.com/@fullcalendar/list@4.4.0/main.min.js'></script>
+
+<script src='https://unpkg.com/@fullcalendar/google-calendar@4.4.0/main.min.js'></script>
+
+
 
 <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: ['interaction', 'dayGrid', 'timeGrid', 'list'],
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-      },
-      defaultDate: '2020-02-12',
-      navLinks: true, // can click day/week names to navigate views
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
 
-      weekNumbers: true,
-      weekNumbersWithinDays: true,
-      weekNumberCalculation: 'ISO',
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'dayGrid', 'list', 'googleCalendar' ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,listYear'
+    },
 
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
+    displayEventTime: false, // don't show the time column in list view
 
-      // Gültiges JSON ist so aufgebaut: 
-      // [
-      //   {
-      //     title: 'All Day Event',
-      //     start: '2020-02-01'
-      //   },
-      //   {
-      //     title: 'Long Event',
-      //     start: '2020-02-07',
-      //     end: '2020-02-10'
-      //   }
-      // ]
+    // THIS KEY WON'T WORK IN PRODUCTION!!!
+    // To make your own Google API key, follow the directions here:
+    // http://fullcalendar.io/docs/google_calendar/
+    googleCalendarApiKey: 'AIzaSyCYHdh89aVp9EA9nh4e_kMDy6ugfVuRHoE',
 
-      //   events: [{
-      //       title: 'All Day Event',
-      //       start: '2020-02-01'
-      //     },
-      //     {
-      //       title: 'Long Event',
-      //       start: '2020-02-07',
-      //       end: '2020-02-10'
-      //     },
-      //     {
-      //       groupId: 999,
-      //       title: 'Repeating Event',
-      //       start: '2020-02-09T16:00:00'
-      //     },
-      //     {
-      //       groupId: 999,
-      //       title: 'Repeating Event',
-      //       start: '2020-02-16T16:00:00'
-      //     },
-      //   ]
-      // });
+    // US Holidays
+    events: 'kooperativegesamtschulerastede@gmail.com',
 
+    eventClick: function(arg) {
 
-      events: [{
-          title: '11. März, 10 bis 12 Uhr',
-          start: '2020-03-11 10:00:00+01:00',
-          end: '2020-03-11 12:00:00+01:00'
-        },
+      // opens events in a popup window
+      window.open(arg.event.url, '_blank', 'width=700,height=600');
 
-        {
-          title: 'Frau Root einweisen',
-          start: '2019-02-06 14:30:00+01:00',
-          end: '2019-02-06 14:45:00+01:00'
-        },
+      // prevents current tab from navigating
+      arg.jsEvent.preventDefault();
+    }
 
-        {
-          title: 'Besprechung Sommerfest',
-          start: '2019-02-27 14:00:00+01:00',
-          end: '2019-02-27 14:30:00+01:00'
-        },
-
-        {
-          title: 'Treffen mit Eltern',
-          start: '2019-02-02 10:00:00+01:00',
-          end: '2019-02-02 12:00:00+01:00'
-        },
-
-        {
-          title: 'Personalversammlung',
-          start: '2019-02-04 14:00:00+01:00',
-          end: '2019-02-04 15:00:00+01:00'
-        },
-
-        {
-          title: 'Büro einräumen',
-          start: '2019-02-01 15:00:00+01:00',
-          end: '2019-02-01 17:00:00+01:00'
-        },
-
-        {
-          title: '2 Tag, 12 und 13. März',
-          start: '2020-03-12',
-          end: '2020-03-14'
-        },
-
-        {
-          title: 'Testevent 1 ganztägig 10. März',
-          start: '2020-03-10',
-          end: '2020-03-11'
-        },
-
-        {
-          title: 'Maker Faire Hannover',
-          start: '2019-08-17 10:00:00+02:00',
-          end: '2019-08-18 11:00:00+02:00'
-        },
-
-        {
-          title: 'Aimo Kohltour',
-          start: '2019-02-22 15:00:00+01:00',
-          end: '2019-02-22 22:00:00+01:00'
-        },
-
-        {
-          title: 'Begehung Räume',
-          start: '2019-04-30',
-          end: '2019-05-01'
-        }
-      ]
-    });
-
-
-
-    calendar.render();
   });
+
+  calendar.render();
+});
+
 </script>
 
 <div id='calendar'></div>
