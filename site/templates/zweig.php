54a40968449bb2b0558178f9a7dbd684914edf30
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-19 22:52:04 +0100

    Seite wird nun generiert

--- site/templates/zweig.php
+++ site/templates/zweig.php
@@ -1,6 +1,9 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
+<?php snippet('sidebar') ?>
+
+
 <div class="container"><?= $page->text()->blocks() ?></div>
 
 <?php snippet('footer') ?>
--- site/templates/zweige.php
+++ site/templates/zweige.php
@@ -2,47 +2,21 @@
 <?php snippet('page-header') ?>
 
 <div class="row">
-  <div class="col-lg-3 col-md-6">
-    <div class="card card-pricing card-background">
-      <div class="card-body">
-        <h3 class="card-category text-info">Hauptschulzweig</h3>
-        <h4 class="card-title">Leitung: Marieke Pannenborg</h4>
+  <?php foreach ($page->children() as $zweig) : ?>
+    <div class="col-lg-3 col-md-6">
+      <div class="card card-pricing card-background">
+        <div class="card-body">
+          <h3 class="card-category text-info"><?= $zweig->title() ?></h3>
+          <h4 class="card-title">Leitung: <?= $zweig->leitung() ?></h4>
 
-        <a href="#pablo" class="btn btn-danger">
-          Weitere Informationen
-        </a>
+          <a href="<?= $zweig->url() ?>" class="btn btn-danger">
+            Weitere Informationen
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="col-lg-3 col-md-6">
-    <div class="card card-pricing card-background">
-      <div class="card-body">
-        <h3 class="card-category text-info">Realschulzweig</h3>
-        <h4 class="card-title">Leitung: Andreas Kleeberg</h4>
-
-        <a href="#pablo" class="btn btn-danger">
-          Weitere Informationen
-        </a>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-lg-3 col-md-6">
-    <div class="card card-pricing card-background">
-      <div class="card-body">
-        <h3 class="card-category text-info">Gymnasialzweig</h3>
-        <h4 class="card-title">Leitung: Malte Bormann</h4>
-
-        <a href="#pablo" class="btn btn-danger">
-          Weitere Informationen
-        </a>
-      </div>
-    </div>
-  </div>
-
+  <?php endforeach ?>
 </div>
 
-<div class="container"><?= $page->text()->blocks() ?></div>
 
 <?php snippet('footer') ?>
