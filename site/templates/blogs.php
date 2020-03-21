c3e91b65374d1d18a9ebb0bc162614f658b47ce9
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-21 18:12:41 +0100

    deutlisch schicker

--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -11,39 +11,38 @@
   </div>
 </section>
 
- 
 
 
-<?php 
+
+<?php
 // `toStructure()` erzeugt einen Iterator fuer die URLs
 $items = $page->children()->flip();
 
 $list = $items->paginate(5);
 
 // Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
-foreach ($list as $item): ?>
- 
- <div class="col-md-12 mt-4">
-    <div class="card">
-        <div class="card-header">
-            <h4 class="card-title">
-                <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
-            </h4>
-            <p class="category">
-                <p class="author">
-                    von
-                    <a href="#">
-                        <b><?= $item->author() ?></b>
-                    </a> Datum: <?= $item->date()->toDate("d.m.Y") ?>
-                </p>
-            </p>
+foreach ($list as $item) : ?>
+
+  <div class="col-md-12 mt-4">
+    <div class="card-body">
+      <h4 class="card-title">
+        <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
+      </h4>
+      <p class="card-category">
+        <div class="author">
+          von
+          <a href="#">
+            <b><?= $item->author() ?></b>
+          </a> Datum: <?= $item->date()->toDate("d.m.Y") ?>
         </div>
-        <div class="card-body">
-            <?= $item->Text()->blocks()->excerpt(250) ?>
-            <a href="<?= $item->url() ?>">...weiterlesen</a>
-        </div>
+      </p>
+      <div class="card-description">
+        <?= $item->Text()->blocks()->excerpt(250) ?>
+        <a href="<?= $item->url() ?>">...weiterlesen</a>
+      </div>
     </div>
-</div>
+  </div>
+  <hr>
 <?php endforeach ?>
 
 
@@ -51,41 +50,41 @@ foreach ($list as $item): ?>
 <nav>
   <ul class="pagination">
 
-    <?php if ($pagination->hasPrevPage()): ?>
-    <li class="page-item">
-      <a class="page-link" href="<?= $pagination->prevPageURL() ?>">‹</a>
-    </li>
-    <?php else: ?>
-    <li>
-      <span>‹</span>
-    </li>
+    <?php if ($pagination->hasPrevPage()) : ?>
+      <li class="page-item">
+        <a class="page-link" href="<?= $pagination->prevPageURL() ?>">‹</a>
+      </li>
+    <?php else : ?>
+      <li>
+        <span>‹</span>
+      </li>
     <?php endif ?>
 
     <!-- Hier nun die mittleren Elemente -->
 
-    <?php foreach ($pagination->range(10) as $r): ?>
-    <li class="page-item <?= $pagination->page() === $r ? 'active' : '' ?>">
-      <!-- Hier steckt hinter:
+    <?php foreach ($pagination->range(10) as $r) : ?>
+      <li class="page-item <?= $pagination->page() === $r ? 'active' : '' ?>">
+        <!-- Hier steckt hinter:
         Wenn page() identisch ist mit $r schreibe aria-current (also aktuelle Seite),
         ansonsten mache '', also füge nichts ein. Das ist eine extreme Abkürzung.
 
         Damit wird im Endeffekt erreicht, dass mit nur einer Zeile die aktuelle Seite
         markiert
       -->
-      <a class="page-link" <?= $pagination->page() === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
-        <?= $r ?>
-      </a>
-    </li>
+        <a class="page-link" <?= $pagination->page() === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
+          <?= $r ?>
+        </a>
+      </li>
     <?php endforeach ?>
 
-    <?php if ($pagination->hasNextPage()): ?>
-    <li class="page-item">
-      <a class="page-link" href="<?= $pagination->nextPageURL() ?>">›</a>
-    </li>
-    <?php else: ?>
-    <li class="page-item">
-      <span>›</span>
-    </li>
+    <?php if ($pagination->hasNextPage()) : ?>
+      <li class="page-item">
+        <a class="page-link" href="<?= $pagination->nextPageURL() ?>">›</a>
+      </li>
+    <?php else : ?>
+      <li class="page-item">
+        <span>›</span>
+      </li>
     <?php endif ?>
 
   </ul>
