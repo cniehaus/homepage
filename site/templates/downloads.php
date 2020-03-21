94c5a031dacb4916488587a4f74bd83df56c50cf
Jesus_ <koala.panda@ewe.net>
2020-03-21 14:50:46 +0100

    adding Download page

---
+++ site/templates/downloads.php
@@ -0,0 +1,39 @@
+<?php snippet('header') ?>
+<?php snippet('page-header') ?>
+
+<h1>Schulbuchlisten</h1>
+
+<div class="d-flex justify-content-around">
+    <div class="card" style="width: 20rem;">
+    <div class="card-body">
+        <h4 class="card-title">Hauptschule</h4>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="javascript:;" class="card-link">Card link</a>
+        <a href="javascript:;" class="card-link">Another link</a>
+    </div>
+    </div>
+
+    <div class="card" style="width: 20rem;">
+    <div class="card-body">
+        <h4 class="card-title">Realschule</h4>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="javascript:;" class="card-link">Card link</a>
+        <a href="javascript:;" class="card-link">Another link</a>
+    </div>
+    </div>
+
+    <div class="card" style="width: 20rem;">
+    <div class="card-body">
+        <h4 class="card-title">Gymnasium</h4>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="javascript:;" class="card-link">Card link</a>
+        <a href="javascript:;" class="card-link">Another link</a>
+    </div>
+    </div>
+</div>
+
+<?php snippet('sidebar') ?>
+<?php snippet('footer') ?>
