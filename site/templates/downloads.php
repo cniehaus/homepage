f15dae29fad2d779815e59448b02b77ae7e6c0c5
Jesus_ <koala.panda@ewe.net>
2020-03-21 15:22:20 +0100

    cards

--- site/templates/downloads.php
+++ site/templates/downloads.php
@@ -1,12 +1,14 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-<h1>Schulbuchlisten</h1>
+<h1>Schulbuchlisten und Leihgebüren 2019/2020</h1>
 
 <div class="d-flex justify-content-around">
     <div class="card" style="width: 20rem;">
-    <div class="card-body">
+    <div class="card-header card-header-danger">
         <h4 class="card-title">Hauptschule</h4>
+    </div>
+    <div class="card-body">
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="javascript:;" class="card-link">Card link</a>
@@ -15,22 +17,44 @@
     </div>
 
     <div class="card" style="width: 20rem;">
-    <div class="card-body">
+    <div class="card-header card-header-danger">
         <h4 class="card-title">Realschule</h4>
+    </div>
+    <div class="card-body">
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="javascript:;" class="card-link">Card link</a>
         <a href="javascript:;" class="card-link">Another link</a>
+        <div class="card card-nav-tabs" style="width: 100%;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        </div>
     </div>
     </div>
 
     <div class="card" style="width: 20rem;">
-    <div class="card-body">
+    <div class="card-header card-header-danger">
         <h4 class="card-title">Gymnasium</h4>
+    </div>
+    <div class="card-body">
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="javascript:;" class="card-link">Card link</a>
-        <a href="javascript:;" class="card-link">Another link</a>
+        <ul>
+            <li>
+                <a href="content\allgemeines\downloads\GZ\schulbuchliste 05GZ.pdf" class="card-link">Anzeigen</a></li>
+            <li>
+                <a href="content\allgemeines\downloads\GZ\schulbuchliste 05GZ.pdf" download="Schulbuchliste-GZ" class="card-link">Download Liste</a></li>
+        </ul>
+        <div class="card card-nav-tabs" style="width: 100%;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        </div>
     </div>
     </div>
 </div>
