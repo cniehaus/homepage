2f1e8dc3843bfbaadf55008ba8fbd9cfd62b8c0c
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-22 08:06:57 +0100

    es liegt nicht gut aus mit dem Logo. Also weg damit

--- site/snippets/teaser-bild.php
+++ site/snippets/teaser-bild.php
@@ -4,7 +4,7 @@
         <?php if ($subpage->hasImages() > 0) : ?>
             <img src="<?= $subpage->images()->first()->url() ?>" class="img img-raised" alt="<?= $subpage->images()->first()->alt() ?>">
         <?php else : ?>
-            <img src="<?= $kirby->url('assets') ?>/logo-kgs.jpg" class="img img-raised" alt="Logo der KGS">
+            <!-- <img src="<?= $kirby->url('assets') ?>/logo-kgs-medium.jpg" class="img img-raised" alt="Logo der KGS"> -->
         <?php endif ?>
 
     </div>
