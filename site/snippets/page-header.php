d122c0aeee37d6c3192bf95858dc828353025d37
Carsten Niehaus <ni@kgs-rastede.de>
2020-04-09 16:18:44 +0200

    das Standardhintergrundbild nun auch beachtet

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,10 +1,24 @@
-<div class="jumbotron" style="background-image: url('<?= $kirby->url('assets') ?>/img/acker2.jpg'); background-size: cover;">
-  <div class="container">
-    <h1 class="title" style="font-size: 3vw"><?= $page->title() ?>
-      <small class="text-muted"><?= $page->subtitle() ?></small>
-    </h1>
+<?php if (page()->hintergrundbild()->isNotEmpty()) : ?>
+
+  <div class="jumbotron" style="background-image: url('<?= $kirby->url() ?>/<?= $page->hintergrundbild() ?>'); background-size: cover;">
+    <div class="container">
+      <h1 class="title" style="font-size: 3vw"><?= $page->title() ?>
+        <small class="text-muted"><?= $page->subtitle() ?></small>
+      </h1>
+    </div>
   </div>
-</div>
+<?php else : ?>
+
+  <div class="jumbotron" style="background-image: url('<?= $kirby->url('assets') ?>/img/acker2.jpg'); background-size: cover;">
+    <div class="container">
+      <h1 class="title" style="font-size: 3vw"><?= $page->title() ?>
+        <small class="text-muted"><?= $page->subtitle() ?></small>
+      </h1>
+    </div>
+  </div>
+
+<?php endif ?>
+
 
 <!-- Ab hier der richtige Inhalt, der auf jeder Seite individuell sein kann -->
 
