e67212d13e9677b10247d6c8217f66413899131b
Carsten Niehaus <ni@kgs-rastede.de>
2020-04-28 21:11:16 +0200

    Kalender klappt nun!

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -2,46 +2,44 @@
 
 <?php snippet('page-header') ?>
 
-
-
-<script src='https://unpkg.com/@fullcalendar/google-calendar@4.4.0/main.min.js'></script>
-
-
+<script src='../../assets/js/packages/core/main.js'></script>
+<script src='../../assets/js/packages/daygrid/main.js'></script>
+<script src='../../assets/js/list/main.js'></script>
 
 <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'de', //Darstellung auf Deutsch
+      plugins: ['list', 'dayGrid'],
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth, dayGridWeek, dayGridDay, listMonth'
+      },
+      buttonText: {
+        today: 'Heute',
+        month: 'Monat',
+        week: 'Woche',
+        day: 'Tag',
+        list: 'Monatsliste',
+      },
+      firstDay: 1,
+      navLinks: true, // can click day/week names to navigate views
+      editable: false,
+      eventLimit: true, // allow "more" link when too many events
+      eventLimitText: "weitere", // allow "more" link when too many events
+      weekNumbers: true, // Kalenderwoche anzeigen
+      weekLabel: "KW", // Beschriftung der Kalenderwoche
+      allDayText: "ganztägig",
+      noEventsMessage: "Keine Ereignisse anzuzeigen",
+      displayEventTime: true, // don't show the time column in list view
+      events: '<?= $kirby->url('assets') ?>/kalender/schulkalender.json'
+    });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    locale: 'de', //Darstellung auf Deutsch
-    plugins: [ 'dayGrid', 'list','googleCalendar' ],
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,listWeek'
-    },
-    buttonText: {
-      today:    'Heute',
-      month:    'Monat',
-      week:     'Woche',
-      day:      'Tag',
-      list:     'Woche',
-    },
-
-    weekNumbers: true, // Kalenderwoche anzeigen
-    weekLabel: "Kalenderwoche", // Beschriftung der Kalenderwoche
-    displayEventTime: true, // don't show the time column in list view
-
-    googleCalendarApiKey: 'AIzaSyCYHdh89aVp9EA9nh4e_kMDy6ugfVuRHoE',
-
-    events: 'kooperativegesamtschulerastede@gmail.com'
-
+    calendar.render();
+    calendar.refetchEvents(); //Test
   });
-
-  calendar.render();
-});
-
 </script>
 
 <div id='calendar'></div>
