e039dad1a4fd6ac5f2786c6f87e635f0f83f6822
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 16:21:20 +0200

    flex grid

--- site/templates/kontakte.php
+++ site/templates/kontakte.php
@@ -4,66 +4,59 @@
 
 
 
-<div class="team-1" id="team-1">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 ml-auto mr-auto text-center">
-                <h2 class="title"><?= $page->title() ?></h2>
-                <h5 class="description">
-                    <?= $page->text() ?>
-                </h5>
-            </div>
-        </div>
-        <div class="row">
-            <?php foreach ($page->children() as $kontakt) : ?>
+<div class="col-md-8 ml-auto mr-auto text-center">
+    <h2 class="title"><?= $page->title() ?></h2>
+    <h5 class="description">
+        <?= $page->text() ?>
+    </h5>
+</div>
 
+<div class="row row-cols-1 row-cols-md-3">
+    <?php foreach ($page->children() as $kontakt) : ?>
+        <div class="col mb-4">
 
-                <div class="col-md-3">
-                    <div class="card">
-                        <img class="card-img-top" src="<?= $kontakt->images()->first()->url() ?>" />                        
-                        <div class="card-body ">
-                            <h4 class="card-title"><?= $kontakt->title() ?></h4>
-                            <h6 class="card-category text-muted"><?= $kontakt->position() ?></h6>
-                        </div>
-
-                        <?php if ($kontakt->phone() != '') : ?>
-                            <div class="card-footer justify-content-center">
-                                <button class="btn btn-success btn-round">
-                                    <img src="<?= $kirby->url('assets') ?>/icons/phone.svg"><?= $kontakt->phone() ?>
-                                </button>
-                            </div>
-                        <?php endif ?>
-
-                        <?php if ($kontakt->email() != '') : ?>
-                            <div class="card-footer justify-content-center">
-                                    <button class="btn btn-success btn-round">
-                                        <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
-
-                                        <script type="text/javascript">
-                                            var mail = "<?= $kontakt->email() ?>";
-                                            var en = "de";
-                                            var dom = "kgs-rastede";
-                                            var at = "@";
-                                            document.open();
-                                            document.write(unescape("%3Ca class='GrosserLink' href='mailto:" + mail + at + dom + "." + en + "'%3E" + mail + at + dom + "." + en + "%3C/a%3E"));
-                                            document.close();
-                                        </script>
-                                    </button>
-                                </a>
-                            </div>
-
-
-
-                        <?php endif ?>
-
-                    </div>
+            <div class="card">
+                <img class="card-img-top" src="<?= $kontakt->images()->first()->url() ?>" />
+                <div class="card-body">
+                    <h5 class="card-title"><?= $kontakt->title() ?></h4>
+                        <p class="card-text"><?= $kontakt->position() ?></h6>
                 </div>
 
-            <?php endforeach ?>
+                <?php if ($kontakt->phone()->isNotEmpty() or $kontakt->email()->isNotEmpty()) : ?>
+                    <div class="card-footer">
+                        <?php if ($kontakt->phone()->isNotEmpty()) : ?>
+                            <p>
+                                <img src="<?= $kirby->url('assets') ?>/icons/phone.svg"><?= $kontakt->phone() ?>
+                            </p>
+                        <?php endif ?>
+
+                        <?php if ($kontakt->email()->isNotEmpty()) : ?>
+                            <p>
+                                <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
+
+                                <script type="text/javascript">
+                                    var mail = "<?= $kontakt->email() ?>";
+                                    var en = "de";
+                                    var dom = "kgs-rastede";
+                                    var at = "@";
+                                    document.open();
+                                    document.write(unescape("%3Ca class='GrosserLink' href='mailto:" + mail + at + dom + "." + en + "'%3E" + mail + at + dom + "." + en + "%3C/a%3E"));
+                                    document.close();
+                                </script>
+                            </p>
+                        <?php endif ?>
+                    </div>
+                <?php endif ?>
+
+            </div>
 
         </div>
-    </div>
+    <?php endforeach ?>
+
 </div>
+
+
+
 
 
 <?php snippet('footer') ?>
