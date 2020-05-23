48021715f218d930263f6f0f9adf64ef616f2690
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 10:53:38 +0200

    reines BS 4.5, keine Ersatzbild

--- site/snippets/teaser-bild.php
+++ site/snippets/teaser-bild.php
@@ -1,11 +1,9 @@
-<div class="col-md-5 mt-4">
-    <div class="card-header">
-
+<div class="col-md-5">
+    
         <?php if ($subpage->hasImages() > 0) : ?>
             <img src="<?= $subpage->images()->first()->url() ?>" class="card-img-top" alt="<?= $subpage->images()->first()->alt() ?>">
         <?php else : ?>
-            <!-- <img src="<?= $kirby->url('assets') ?>/logo-kgs-medium.jpg" class="img img-raised" alt="Logo der KGS"> -->
         <?php endif ?>
 
-    </div>
+    
 </div>
