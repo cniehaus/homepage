bb3b6ccd918b72ebbbaae123434b35c29a2582cd
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 17:10:00 +0200

    anpassungen an CSS

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -1,67 +1,61 @@
 <div class="container-fluid">
-        <?php if ($page->sidebar()->isNotEmpty() or $page->related()->isNotEmpty() ) : ?>
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="container"><?= $page->text()->blocks() ?></div>
-                </div>
+    <?php if ($page->sidebar()->isNotEmpty() or $page->related()->isNotEmpty()) : ?>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="container"><?= $page->text()->blocks() ?></div>
+            </div>
 
+            <div class="col-mb-3">
                 <?php if ($page->sidebar()->isNotEmpty()) : ?>
 
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-category text-primary">
-                            <h5 class="card-title">
-                                <?php if ($page->sidetitel()->isNotEmpty()) : ?>
-                                    <?= $page->sidetitel()->kirbytext() ?>
-                                <?php else : ?>
-                                    <?= "Weitere Informationen" ?>
-                                <?php endif ?>
-                            </h5>
-                        </div>
+                    <div class="card mb-3">
+                        <h5 class="card-title">
+                            <?php if ($page->sidetitel()->isNotEmpty()) : ?>
+                                <?= $page->sidetitel()->kirbytext() ?>
+                            <?php else : ?>
+                                <?= "Weitere Informationen" ?>  
+                            <?php endif ?>
+                        </h5>
                         <div class="card-body">
                             <ul class="list-group list-group-flush">
                                 <?php foreach ($page->sidebar()->toStructure() as $sidebar) : ?>
-                                    <li class="list-group-item text-left">
-                                        <a href="<?= $sidebar->link() ?>" class="list-group-item list-group-item-action"><?= $sidebar->name() ?></a>
+                                    <li class="list-group-item">
+                                        <a href="<?= $sidebar->link() ?>"><?= $sidebar->name() ?></a>
                                     </li>
                                 <?php endforeach ?>
                             </ul>
                         </div>
                     </div>
 
-                    <?php endif ?>
+                <?php endif ?>
 
 
-                    <?php if ($page->related()->isNotEmpty() ) : ?>
+                <?php if ($page->related()->isNotEmpty()) : ?>
 
 
-                    <div class="card">
-                        <div class="card-category text-primary">
-                            <h5 class="card-title">
-                                Weitere Informationen
-                            </h5>
-                        </div>
+                    <div class="card mb-3">
+                        <h5 class="card-title">
+                            Weitere Informationen
+                        </h5>
                         <div class="card-body">
                             <ul class="list-group list-group-flush">
                                 <?php $relatedPages =  $page->related()->toPages();
                                 foreach ($relatedPages as $relatedPage) : ?>
-                                
-                                <li class="list-group-item text-left">
-                                    <a href="<?= $relatedPage->url() ?>"><?= $relatedPage->title() ?></a>
-                                </li>
+                                    <li class="list-group-item">
+                                        <a href="<?= $relatedPage->url() ?>"><?= $relatedPage->title() ?></a>
+                                    </li>
                                 <?php endforeach ?>
 
                             </ul>
                         </div>
                     </div>
 
-                    <?php endif ?>
+                <?php endif ?>
 
-
-                </div>
 
             </div>
-        <?php else : ?>
-            <div class="container"><?= $page->text()->blocks() ?></div>
-        <?php endif ?>
+        </div>
+    <?php else : ?>
+        <div class="container"><?= $page->text()->blocks() ?></div>
+    <?php endif ?>
 </div>
