06dc6f36398ec528cdc96618bea9a94f8b772a17
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 10:36:13 +0200

    reine Bootstrap-Klassen

--- site/snippets/teaser-bild-text.php
+++ site/snippets/teaser-bild-text.php
@@ -3,17 +3,13 @@
         <h4 class="card-title">
             <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
         </h4>
-        <p class="card-category">
-            <div class="author">
-                von
-                <a href="#">
-                    <b><?= $subpage->author() ?></b>
-                </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
-            </div>
+        <p>
+            von <b><?= $subpage->author() ?></b>
+            Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
         </p>
-        <div class="card-description">
+        <p class="card-text">
             <?= $subpage->Text()->blocks()->excerpt(250) ?>
-            <a href="<?=     $subpage->url() ?>">...weiterlesen</a>
-        </div>
+            <a href="<?= $subpage->url() ?>" class="card-link">...weiterlesen</a>
+        </p>
     </div>
 </div>
