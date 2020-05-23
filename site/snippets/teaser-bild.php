c4afe348dae0b5a32ac499d3a21a55d2fa18ae86
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 10:08:54 +0200

    Bilder in Blogs gehen wieder.

--- site/snippets/teaser-bild.php
+++ site/snippets/teaser-bild.php
@@ -1,8 +1,8 @@
 <div class="col-md-5 mt-4">
-    <div class="card-header card-header-image">
+    <div class="card-header">
 
         <?php if ($subpage->hasImages() > 0) : ?>
-            <img src="<?= $subpage->images()->first()->url() ?>" class="img img-raised" alt="<?= $subpage->images()->first()->alt() ?>">
+            <img src="<?= $subpage->images()->first()->url() ?>" class="card-img-top" alt="<?= $subpage->images()->first()->alt() ?>">
         <?php else : ?>
             <!-- <img src="<?= $kirby->url('assets') ?>/logo-kgs-medium.jpg" class="img img-raised" alt="Logo der KGS"> -->
         <?php endif ?>
