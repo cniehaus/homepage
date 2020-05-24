2cd5be32f36df408001b93290b26d5a901056fff
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-24 10:54:05 +0200

    selbst kompiliertes .css

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,9 +7,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
-  <link rel="stylesheet" href="<?= $kirby->url('assets') ?>/css/bootstrap.min.css" >
-
-  <link rel="stylesheet" href="<?= $kirby->url('assets') ?>/css/kgs.css" >
+  <link rel="stylesheet" href="<?= $kirby->url('assets') ?>/css/kgs.min.css" >
 
 </head>
 
