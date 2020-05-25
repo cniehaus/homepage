29acb1f97c330c5625c2f06909d167874eb7203f
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-25 15:57:42 +0200

    etwas besseresHTML...

--- site/templates/impressum.php
+++ site/templates/impressum.php
@@ -4,31 +4,31 @@
 
 
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-5">
         <h3><b>Anschrift</b></h3>
-        <p style="letter-spacing:0.01em; word-spacing:0.3em; line-height:1.3em;"><h5> 
-            <?= $page->anschrift() ?>  
+        <h5>
+            <?= $page->anschrift() ?>
             Tel.: <?= $page->telefon()->tel() ?> </br>
             Fax: <?= $page->fax()->tel() ?> </br>
             E-Mail: <span style="text-decoration:underline;"><a href="mailto:<?= $page->email() ?>"><?= $page->email()->email() ?></a></span></br>
-        </h5></p>
-        <h6>(gem. §5 Telemediengesetz)</h6> 
+        </h5>
+        <h6>(gem. §5 Telemediengesetz)</h6>
         <h3><b>Inhaltlich Verantwortlich</b></h3>
-        <p style="letter-spacing:0.01em; word-spacing:0.3em; line-height:1.3em;"><h5>
+        <h5>
             <?= $page->inhaltlichVerantwortlich()->text() ?>
-        </h5></p>       
+        </h5>
     </div>
-    
-    <div class="col-md-4">
+
+    <div class="col-md-5">
         <h3><b>Schulleitung</b></h3>
-            <table class="table">
+        <table class="table">
             <tbody>
                 <tr>
                     <td class="text-left"><?= $page->schulleiter()->text() ?></td>
                     <td>
-                        <?php if ($page->schulleiterGeschlecht() == "frau"): ?>
+                        <?php if ($page->schulleiterGeschlecht() == "frau") : ?>
                             Schulleiterin
-                        <?php else: ?>
+                        <?php else : ?>
                             Schulleiter
                         <?php endif ?>
                     </td>
@@ -38,31 +38,31 @@
                                 <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
                             </button>
                         </a>
-                    </td>                
+                    </td>
                 </tr>
                 <tr>
                     <td class="text-left"><?= $page->stellver()->text() ?></td>
                     <td>
-                        <?php if ($page->stellverGeschlecht() == "frau"): ?>
+                        <?php if ($page->stellverGeschlecht() == "frau") : ?>
                             Stellv. Schulleiterin
-                        <?php else: ?>
+                        <?php else : ?>
                             Stellv. Schulleiter
                         <?php endif ?>
                     </td>
                     <td class="td-actions text-right">
                         <a href="mailto:<?= $page->stellvermail() ?>">
                             <button type="button" rel="tooltip" class="btn btn-info">
-                               <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
+                                <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
                             </button>
                         </a>
-                    </td>                
+                    </td>
                 </tr>
                 <tr>
                     <td class="text-left"><?= $page->didaktischeLeitung()->text() ?></td>
                     <td>
-                        <?php if ($page->didaktischeLeitungGeschlecht() == "frau"): ?>
+                        <?php if ($page->didaktischeLeitungGeschlecht() == "frau") : ?>
                             Didaktische Leiterin
-                        <?php else: ?>
+                        <?php else : ?>
                             Didaktischer Leiter
                         <?php endif ?>
                     </td>
@@ -72,29 +72,26 @@
                                 <img src="<?= $kirby->url('assets') ?>/icons/envelope.svg">
                             </button>
                         </a>
-                    </td>                
+                    </td>
                 </tr>
             </tbody>
-        </table> 
-        </br>        
+        </table>
+        </br>
         <h3><b>Schulträger</b></h3>
         <p style="letter-spacing:0.01em; word-spacing:0.3em; line-height:1.3em;">
-            <h5><?= $page->schultrager()->text() ?></h5>  
+            <h5><?= $page->schultrager()->text() ?></h5>
         </p>
     </div>
-    <!-- # Texte unter den infos -->
-    <div class="col-md-7">
-    <?php         
-        $items = $page->texte()->toStructure();
-       
-        foreach ($items as $item): ?>
-            <h3><b><?= $item->ueberschrift()->text() ?></b></h3>
-            <?= $item->text()->text() ?></br>       
-        <?php endforeach ?>
-    </div>
+</div>
+<!-- # Texte unter den Infos -->
+<div class="col-md-10">
+    <?php $items = $page->texte()->toStructure();
+    foreach ($items as $item) : ?>
+        <h3><b><?= $item->ueberschrift()->text() ?></b></h3>
+        <?= $item->text()->text() ?></br>
+    <?php endforeach ?>
 </div>
 
 
-</div>
 
 <?php snippet('footer') ?>
