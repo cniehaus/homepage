2211d1ebbe8d3a260b08e05fa4c97b9052f89374
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-25 11:54:27 +0200

    vereinfachtes Layout

--- site/snippets/box-presse.php
+++ site/snippets/box-presse.php
@@ -1,18 +1,17 @@
 <div class="card mb-1">
     <div class="card-body ">
         <h4 class="card-category text-primary">Aus der Presse</h4>
-        <ul>
-            <?php
-            $items = page('allgemeines/schulstruktur/presse')->pressenachrichten()->toStructure()->sortBy("datum")->flip()->limit(6);
+        <?php
+        $items = page('allgemeines/schulstruktur/presse')->pressenachrichten()->toStructure()->sortBy("datum")->flip()->limit(6);
 
-            foreach ($items as $item) : ?>
-                <li>
-                    <a class="card-link" href="<?= $item->link() ?>"><?= $item->name() ?></a>
-                    (<?= $item->datum()->toDate('d.m.y') ?>)
-                </li>
+        foreach ($items as $item) : ?>
+            <p>
+                <a class="card-link" href="<?= $item->link() ?>"><?= $item->name() ?></a>
+                (<?= $item->datum()->toDate('d.m.y') ?>)
+            </p>
 
 
-            <?php endforeach ?>
-        </ul>
+        <?php endforeach ?>
+
     </div>
 </div>
