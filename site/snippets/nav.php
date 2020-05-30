6eeccae64c6c6cbf3c9cfc980720eb4186691ffb
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-30 14:29:58 +0200

    nichtlehrendes Personal auf einer Seite

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -26,10 +26,10 @@
         <div class="dropdown-menu dropdown-with-icons">
           <a class="dropdown-item" href="<?= page('allgemeines/anfahrt')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Anfahrt</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?= page('kontakte/hausmeister')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Hausmeister</a>
+          <a class="dropdown-item" href="<?= page('kontakte/sekretariate')->url() ?>#haus"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Hausmeister</a>
           <a class="dropdown-item" href="<?= page('kontakte/schulleitung')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Schulleitung</a>
           <a class="dropdown-item" href="<?= page('kontakte/fbl')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Fachbereichsleiter</a>
-          <a class="dropdown-item" href="<?= page('kontakte/sekretariate')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Sekretariate</a>
+          <a class="dropdown-item" href="<?= page('kontakte/sekretariate')->url() ?>#sek"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Sekretariate</a>
           <a class="dropdown-item" href="<?= page('lehrer')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">das Kollegium</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="<?= page('sv/die_sv')->url() ?>"><img src="<?= $kirby->url('assets') ?>/icons/people-fill.svg">Schülervertretung (SV)</a>
--- site/templates/kontakte-tabelle.php
+++ site/templates/kontakte-tabelle.php
@@ -13,6 +13,48 @@
     <?= $page->text() ?>
 </h5>
 
+
+<h1 id="sek">Sekretariate</h1>
+
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">Aufgabenbereich</th>
+            <th scope="col">Name</th>
+            <th scope="col">Telefonnummer</th>
+            <th scope="col">Emailadresse</th>
+        </tr>
+    </thead>
+    <tbody>
+         <?php foreach ($page->children() as $kontakt) : ?>
+            <?php
+            if ($kontakt->fotoansicht() != "sek") continue;
+            ?>
+
+            <tr>
+                <td><?= $kontakt->position() ?></td>
+                <td><?= $kontakt->title() ?></td>
+                <td><?= $kontakt->phone() ?></td>
+                <td>
+                    <script type="text/javascript">
+                        var mail = "<?= $kontakt->email() ?>";
+                        var en = "de";
+                        var dom = "kgs-rastede";
+                        var at = "@";
+                        document.open();
+                        document.write(unescape("%3Ca href='mailto:" + mail + at + dom + "." + en + "'%3E" + mail + at + dom + "." + en + "%3C/a%3E"));
+                        document.close();
+                    </script>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+
+
+
+<h1  id="haus">Hausmeister und Schulassistenten</h1>
+
 <table class="table table-striped">
     <thead>
         <tr>
@@ -24,17 +66,24 @@
     </thead>
     <tbody>
         <?php foreach ($page->children() as $kontakt) : ?>
+            <?php
+            if ($kontakt->fotoansicht() != "hausmeister") {
+                continue;
+            }
+            ?>
+
             <tr>
                 <td><?= $kontakt->position() ?></td>
                 <td><?= $kontakt->title() ?></td>
                 <td><?= $kontakt->phone() ?></td>
-                <td><script type="text/javascript">
+                <td>
+                    <script type="text/javascript">
                         var mail = "<?= $kontakt->email() ?>";
                         var en = "de";
                         var dom = "kgs-rastede";
                         var at = "@";
                         document.open();
-                        document.write(unescape("%3Ca class='GrosserLink' href='mailto:" + mail + at + dom + "." + en + "'%3E" + mail + at + dom + "." + en + "%3C/a%3E"));
+                        document.write(unescape("%3Ca href='mailto:" + mail + at + dom + "." + en + "'%3E" + mail + at + dom + "." + en + "%3C/a%3E"));
                         document.close();
                     </script>
                 </td>
@@ -42,7 +91,6 @@
         <?php endforeach ?>
     </tbody>
 </table>
-
 
 
 
