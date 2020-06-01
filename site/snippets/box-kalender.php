9ea8760547cf93f66bef7bb755681f28d049147a
Carsten Niehaus <ni@kgs-rastede.de>
2020-06-01 21:25:22 +0200

    Kalender der Seitenleiste

---
+++ site/snippets/box-kalender.php
@@ -0,0 +1,49 @@
+<div class="card bg-primary my-4">
+    <link href='<?= $kirby->url('assets') ?>/js/plugins/main.css' rel='stylesheet' />
+
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
+
+
+    <script src='<?= $kirby->url('assets') ?>/js/plugins/main.js'></script>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                locale: 'de', //Darstellung auf Deutsch
+                height: 'auto',
+
+                initialView: 'listWeek',
+
+
+                headerToolbar: {
+                    left: '',
+                    center: 'title',
+                    right: ''
+                },
+
+                // buttonText: {
+                //     today: 'Heute',
+                //     month: 'Monat',
+                //     week: 'Woche',
+                //     day: 'Tag',
+                //     list: 'Monatsliste',
+                // },
+                firstDay: 1,
+                weekLabel: "KW", // Beschriftung der Kalenderwoche
+                allDayText: "ganztägig",
+                noEventsMessage: "Keine Ereignisse anzuzeigen",
+                displayEventTime: true, // don't show the time column in list view
+                events: '<?= $kirby->url('assets') ?>/kalender/schulkalender.json'
+            });
+
+            calendar.render();
+            calendar.refetchEvents(); //Test
+        });
+    </script>
+
+    <div id='calendar'></div>
+</div>
