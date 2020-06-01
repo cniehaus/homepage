c6c5c30339eb955257785a1179cfbb059d2912a0
Carsten Niehaus <ni@kgs-rastede.de>
2020-06-01 21:30:05 +0200

    IF-Bedingungen vereinfachen

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -2,17 +2,10 @@
 
 <?php if ($image = $page->hintergrundbild()->toFile()) : ?>
   <div class="jumbotron jumbotron-fluid" style="background-image: url('<?= $image->url() ?>'); background-size: cover;">
-    <div class="titelfeld">
-      <div class="container">
-        <h1><?= $page->title() ?>
-          <small class="text-muted"><?= $page->subtitle() ?></small>
-        </h1>
-      </div>
-    </div>
-  </div>
 <?php else : ?>
-
   <div class="jumbotron jumbotron-fluid" style="background-image: url('<?= $kirby->url('assets') ?>/img/banner_eng.jpg'); background-size: cover;">
+<?php endif ?>
+
     <div class="titelfeld">
       <div class="container">
         <h1><?= $page->title() ?>
@@ -22,6 +15,5 @@
     </div>
   </div>
 
-<?php endif ?>
 
 <!-- Ab hier der richtige Inhalt, der auf jeder Seite individuell sein kann -->
