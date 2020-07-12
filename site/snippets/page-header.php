c8b24fae6f8e9aed2f8da62c77bf527be8a68ec3
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-12 11:20:47 +0200

    Bild vernünftig einbinden. Noch nicht perfekt

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,29 +1,34 @@
 <main role="main">
 
 
-  <?php
-
-  $bildpfad = "";
-
-  if ($image = $page->hintergrundbild()->toFile()) : ?>
-    $bildpfad = <?= $image->url() ?>;
-  <?php else : ?>
-    $bildpfad = <?= $kirby->url('assets') ?> + "/img/banner_eng.jpg";
-  <?php endif ?>
 
 
 
 
-  <div class="p-5 text-center bg-image" style="
-      background-image: url('https://mdbootstrap.com/img/new/slides/041.jpg');
-      height: 400px;
-    ">
+
+    <?php
+    if ($image = $page->hintergrundbild()->toFile()) : ?>
+
+
+      <div class="p-5 text-center bg-image" style="
+            background-image: url('<?= $image->url() ?>');
+            height: 400px;">
+
+      
+    <?php else : ?>
+      
+      <div class="p-5 text-center bg-image" style="
+      background-image: url('<?= $kirby->url('assets') ?>/img/banner_eng.jpg');
+      ">
+      
+    <?php endif ?>
+
+
     <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
       <div class="d-flex justify-content-center align-items-center h-100">
         <div class="text-white">
           <h1 class="mb-3"><?= $page->title() ?></h1>
           <h4 class="mb-3"><?= $page->subtitle() ?></h4>
-          <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
         </div>
       </div>
     </div>
