08ed72fff9423eb014bbed00de9a9d9015600c98
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-22 22:36:53 +0200

    beide Blogs nutzten nun den selben Code!

--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -3,38 +3,14 @@
 <?php snippet('page-header') ?>
 
 <?php
-// `toStructure()` erzeugt einen Iterator fuer die URLs
-$items = $page->children()->flip();
+  $items = $page->children()->flip();
 
-$list = $items->paginate(5);
+  $list = $items->paginate(5);
 
-// Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
-foreach ($list as $item) : ?>
-
-  <div class="col-md-12 mt-4">
-    <div class="card">
-    <div class="card-body">
-      <h4 class="card-title">
-        <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
-      </h4>
-      <p class="card-category">
-        <div class="author text-right">
-          von
-            <b><?= $item->author() ?></b>
-          Datum: <?= $item->date()->toDate("d.m.Y") ?>
-        </div>
-      </p>
-      <p class="card-text">
-        <?= $item->Text()->blocks()->excerpt(250) ?>
-      </p>
-      <a href="<?= $item->url() ?>" class="btn btn-secondary">weiterlesen &#8594;
-</a>          
-    </div>
-  </div>
-  
-  </div>
-  <hr>
-<?php endforeach ?>
+  snippet('blog-schlicht', [
+    'items' => $list
+  ])
+?>
 
 
 <?php $pagination = $list->pagination() ?>
--- site/templates/ser.php
+++ site/templates/ser.php
@@ -7,43 +7,62 @@
 
 
 
-<?php
-// `toStructure()` erzeugt einen Iterator fuer die URLs
-$items = page('ser/nachrichten')->children()->listed();
 
-$list = $items->paginate(5);
+  <?php
+    $items = page('ser/nachrichten')->children()->listed();
 
-// Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
-foreach ($list as $item) : ?>
+    $list = $items->paginate(3);
 
-  <div class="col-md-12 mt-4">
-    <div class="card">
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
-        </h4>
-        <p class="card-category">
-          <div class="author text-right">
-            von
-            <b><?= $item->author() ?></b>
-            Datum: <?= $item->date()->toDate("d.m.Y") ?>
-          </div>
-        </p>
-        <p class="card-text">
-          <?= $item->Text()->blocks()->excerpt(250) ?>
-        </p>
-        <a href="<?= $item->url() ?>" class="btn btn-secondary">weiterlesen &#8594;
-        </a>
-      </div>
-    </div>
-
-  </div>
-  <hr>
-<?php endforeach ?>
+    snippet('blog-schlicht', [
+      'items' => $list
+    ])
+  ?>
 
 
+  <?php $pagination = $list->pagination() ?>
+  <nav>
+    <ul class="pagination">
+
+      <?php if ($pagination->hasPrevPage()) : ?>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pagination->prevPageURL() ?>">‹</a>
+        </li>
+      <?php else : ?>
+        <li>
+          <span>‹</span>
+        </li>
+      <?php endif ?>
+
+      <!-- Hier nun die mittleren Elemente -->
+
+      <?php foreach ($pagination->range(10) as $r) : ?>
+        <li class="page-item <?= $pagination->page() === $r ? 'active' : '' ?>">
+          <!-- Hier steckt hinter:
+        Wenn page() identisch ist mit $r schreibe aria-current (also aktuelle Seite),
+        ansonsten mache '', also füge nichts ein. Das ist eine extreme Abkürzung.
+
+        Damit wird im Endeffekt erreicht, dass mit nur einer Zeile die aktuelle Seite
+        markiert
+      -->
+          <a class="page-link" <?= $pagination->page() === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
+            <?= $r ?>
+          </a>
+        </li>
+      <?php endforeach ?>
+
+      <?php if ($pagination->hasNextPage()) : ?>
+        <li class="page-item">
+          <a class="page-link" href="<?= $pagination->nextPageURL() ?>">›</a>
+        </li>
+      <?php else : ?>
+        <li class="page-item">
+          <span>›</span>
+        </li>
+      <?php endif ?>
+
+    </ul>
+  </nav>
 
 
 
-
-<?php snippet('footer') ?>
+  <?php snippet('footer') ?>
