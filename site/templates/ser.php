70d2e1bb3c43c40c389e93a452f6b6918a4c4771
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-22 22:49:40 +0200

    deutlich weniger Code, nutzt das neue Snippet

--- site/templates/ser.php
+++ site/templates/ser.php
@@ -7,62 +7,25 @@
 
 
 
+<?php
+  $items = page('ser/nachrichten')->children()->listed();
 
-  <?php
-    $items = page('ser/nachrichten')->children()->listed();
+  $list = $items->paginate(3);
 
-    $list = $items->paginate(3);
+  snippet('blog-schlicht', [
+    'items' => $list
+  ])
 
-    snippet('blog-schlicht', [
-      'items' => $list
-    ])
-  ?>
-
-
-  <?php $pagination = $list->pagination() ?>
-  <nav>
-    <ul class="pagination">
-
-      <?php if ($pagination->hasPrevPage()) : ?>
-        <li class="page-item">
-          <a class="page-link" href="<?= $pagination->prevPageURL() ?>">‹</a>
-        </li>
-      <?php else : ?>
-        <li>
-          <span>‹</span>
-        </li>
-      <?php endif ?>
-
-      <!-- Hier nun die mittleren Elemente -->
-
-      <?php foreach ($pagination->range(10) as $r) : ?>
-        <li class="page-item <?= $pagination->page() === $r ? 'active' : '' ?>">
-          <!-- Hier steckt hinter:
-        Wenn page() identisch ist mit $r schreibe aria-current (also aktuelle Seite),
-        ansonsten mache '', also füge nichts ein. Das ist eine extreme Abkürzung.
-
-        Damit wird im Endeffekt erreicht, dass mit nur einer Zeile die aktuelle Seite
-        markiert
-      -->
-          <a class="page-link" <?= $pagination->page() === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
-            <?= $r ?>
-          </a>
-        </li>
-      <?php endforeach ?>
-
-      <?php if ($pagination->hasNextPage()) : ?>
-        <li class="page-item">
-          <a class="page-link" href="<?= $pagination->nextPageURL() ?>">›</a>
-        </li>
-      <?php else : ?>
-        <li class="page-item">
-          <span>›</span>
-        </li>
-      <?php endif ?>
-
-    </ul>
-  </nav>
+?>
 
 
+<?php
+  $pagination = $list->pagination();
 
-  <?php snippet('footer') ?>
+  snippet('pagination', [
+    'pagination' => $pagination
+  ])
+
+?>
+
+<?php snippet('footer') ?>
