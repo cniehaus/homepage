e3384df4244da32757180f95d399d915fdf94c32
Ole <ole.bohmbach@gmail.com>
2020-07-25 12:47:56 +0200

    Ohne Text sieht es besser aus

--- site/snippets/box-foerderverein.php
+++ site/snippets/box-foerderverein.php
@@ -1,6 +1,6 @@
 <div class="card mb-1">
     <div class="card-body">
-        <h4 class="card-category">Der Förderverein</h4>
+        <h4 class="card-category"></h4>
         <ul>
         <a href="<?= page('foerderverein/vorstand')->url() ?>">
                 <img src="<?= $kirby->url('assets') ?>/bilder/foerderverein.png" class="img-fluid">
