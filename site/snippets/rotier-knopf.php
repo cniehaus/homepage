eea1ad4062be818f399ac96339ed10850504ebe8
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-31 12:13:43 +0200

    noch ein kaputtes Logo

--- site/snippets/rotier-knopf.php
+++ site/snippets/rotier-knopf.php
@@ -2,10 +2,16 @@
     <div class="card-body">
         <h3 class="card-title"><?= $subpage->titel() ?></h3>
         <p class="card-text"><?= $subpage->text() ?></p>
-        <?php if($file = $subpage->datei()->toFile()): ?>
-            <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-primary"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg"><?php echo $file->name() ?></a>
+        <?php if ($file = $subpage->datei()->toFile()) : ?>
+            <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-primary">
+                <svg class="bi" width="24" height="24">
+                    <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#info-square" />
+                </svg>
+                <?php echo $file->name() ?>
+
+            </a>
         <?php endif ?>
-        <?php if($subpage->link() != ""): ?>
+        <?php if ($subpage->link() != "") : ?>
             <a href="<?= $subpage->link() ?>" class="btn btn-primary" role="button"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg">Mehr zum Thema</a>
         <?php endif ?>
     </div>
