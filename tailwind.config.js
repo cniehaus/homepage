2cef71894c0be1a63a32c17c42dcf76795aa1da4
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-09 16:49:50 +0200

    Farben für Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#f9f8f8',
-        secondary: '586994',
-        success: '#bdc8d4',
-        info: '#0099cc',
-        warning: '#a34c40',
-        danger: '#8c271e',
-        light: '#0e5499',
-        dark: '#c0c0c0',
+        "primary": '#f9f8f8',
+        "secondary": '586994',
+        "success": '#bdc8d4',
+        "info": '#0099cc',
+        "warning": '#a34c40',
+        "danger": '#8c271e',
+        "light": '#0e5499',
+        "dark": '#c0c0c0',
       }
     },
     // hier weiß ich nicht, wieso das nicht geht... Dit Fonts sind lokal installiert.
     // Eigentlich müsste es gehen...
     // https://stackoverflow.com/questions/61976785/how-do-you-use-custom-fonts-with-tailwindcss-and-nuxtjs
     fontFamily: {
-      body: ['Roboto']
+      'roboto': ['Roboto', 'sans-serif'],
+      'work-sans': ['Work Sans', 'sans-serif'],
     }
   },
   variants: {},
