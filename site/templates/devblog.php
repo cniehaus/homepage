d6499d9b8b34ef5d88b08940268efe9d28b96e9b
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-13 11:06:59 +0200

    der Blog hat nun ein modernes Gird, IE 11 kann das wohl nicht, aber ich möchte keine Steinzeittechnik unterstütuzen

--- site/templates/devblog.php
+++ site/templates/devblog.php
@@ -31,7 +31,7 @@
                             <p class="mt-2 text-gray-600">
                                 <?= $a->Text()->blocks()->excerpt(250) ?>
                             </p>
-                            <div class="mt-4">
+                            <div class="mt-4 mt-">
                                 <a href="<?= $a->url() ?>" class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 hover:bg-indigo-400 text-sm text-white uppercase tracking-wider font-semibold">Weiterlesen</a>
                             </div>
                         </div>
@@ -42,14 +42,16 @@
 
             <hr class="border-b-2 mt-6 border-gray-400 mb-8 mx-4">
 
+            <div class="max-w-screen-xl mx-auto px-2">
+                <!-- Grid wrapper -->
+                <div class="-mx-10 flex flex-wrap">
+                    <?php foreach ($articles as $a) : ?>
 
-            <div class="grid grid-flow-row sm:grid-flow-row md:grid-flow-col">
-                <?php foreach ($articles as $a) : ?>
-
-                    <div class="pt-5">
-
-                        <div class="max-w-lg mx-2 rounded overflow-hidden shadow-lg bg-gray-300">
-                            <div class="col p-4 d-flex flex-column position-static">
+                        <!-- Grid column -->
+                        <div class="w-full flex flex-col p-2 sm:w-1/2 lg:w-1/3">
+                            <!-- Column contents -->
+                            <div class="flex-1 px-4 py-12 bg-gray-300 rounded-lg shadow-lg">
+                                <!-- Card contents -->
                                 <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight">
                                     <?= $a->title() ?>
                                 </h1>
@@ -63,15 +65,11 @@
 
                                 </div>
 
-                                <div class="p-6">
-                                    <div class="flex items-center">
-
-                                        <svg class="bi" width="24" height="24">
-                                            <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#tags" />
-                                        </svg>
+                                <div class="p-2">
+                                    <div class="px-1 py-1">
                                         <?php foreach ($a->tags()->split() as $tag) : ?>
                                             <a href="<?= url('devblog', ['params' => ['tag' => $tag]]) ?>">
-                                                <span class="inline-block bg-blue-200 text-blue-800 text-lg px-2 rounded-full uppercase font-semibold tracking-wide"><?= $tag ?></span>
+                                                <span class="inline-block bg-blue-200 text-blue-800 text-sm px-2 rounded-full uppercase font-semibold tracking-wide"><?= $tag ?></span>
                                             </a>
                                         <?php endforeach ?>
                                     </div>
@@ -81,16 +79,16 @@
                                 <p class="card-text mb-auto">
                                     <?= $a->Text()->blocks()->excerpt(250) ?>
                                 </p>
-                                <a href="<?= $a->url() ?>" class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 hover:bg-indigo-400 text-sm text-white uppercase tracking-wider font-semibold">Weiterlesen</a>
-
-
+                                <div class="mt-10">
+                                    <a href="<?= $a->url() ?>" class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 hover:bg-indigo-400 text-sm text-white uppercase tracking-wider font-semibold">Weiterlesen</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endforeach ?>
 
-                <?php endforeach ?>
-
+                </div>
             </div>
+
 
         </div>
         <div class="w-1/4 bg-gray-400">
