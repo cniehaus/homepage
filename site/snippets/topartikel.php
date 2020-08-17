8cd17e99a9df910e94fafd5adf8076ebdca54f11
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-17 19:16:46 +0200

    etwas Platzsparender

--- site/snippets/topartikel.php
+++ site/snippets/topartikel.php
@@ -1,4 +1,4 @@
-<div class="card p-3 mt-3 mb-3 bg-gradient bg-success text-black">
+<div class="card mt-3 mb-3 bg-gradient bg-success text-black">
     <div class="card-body">
         <h3 class="card-title"><?= $subpage->title() ?></h3>
 
@@ -11,11 +11,12 @@
 
                 <h5 class="lead mt-5 text-black">Passende Downloads</h5>
 
+
                 <?php $dateien = $subpage->downloads()->toFiles();
                 foreach ($dateien as $datei) : ?>
 
 
-                    <a href="<?php echo $datei->url() ?>" download="<?php echo $datei->filename() ?>" class="btn btn-primary m-2">
+                    <a href="<?php echo $datei->url() ?>" download="<?php echo $datei->filename() ?>" class="btn btn-primary mt-2">
                         <svg class="bi" width="24" height="24">
                             <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#info-square" />
                         </svg>
@@ -24,7 +25,6 @@
                     </a>
 
                 <?php endforeach ?>
-
 
             <?php endif ?>
         </div>
