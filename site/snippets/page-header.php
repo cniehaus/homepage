a9d7e6d3d2c335315c0a02e7cb60f0fdc8c64557
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-22 17:13:29 +0200

    deutlich frischeres Design des TItels

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,32 +1,31 @@
-
 <main role="main">
   <?php
-    if ($image = $page->hintergrundbild()->toFile()) : ?>
+  if ($image = $page->hintergrundbild()->toFile()) : ?>
 
-      <div class="p-5 text-center bg-image" style="
+    <div class="p-5 text-center bg-image" style="
             background-image: url('<?= $image->url() ?>');
             height: 400px;">
 
-  <?php else : ?>
-    <div class="p-5 mt-5 text-center bg-image" style="
+    <?php else : ?>
+      <div class="p-5 mt-5 text-center bg-image" style="
     background-image: url('<?= $kirby->url('assets') ?>/img/banner_eng.jpg');
     ">
-  <?php endif ?>
-</div>
+      <?php endif ?>
+      </div>
 
 
 
 
-<!-- Titelfeld -->
-  <div class="titelfeld">
-    <div class="container">
-      <h1>
-        <?= $page->title() ?>
-      </h1>
-        <small class="text-muted"><?= $page->subtitle() ?></small>
+      <!-- Titelfeld -->
+
+      <div class="p-3 mb-2 bg-light text-primary">
+        <h1>
+          <?= $page->title() ?>
+        </h1>
+      </div>
+      <small class="text-muted"><?= $page->subtitle() ?></small>
 
       <p class="lead">
         <?= $page->heading() ?>
       </p>
     </div>
-  </div>
