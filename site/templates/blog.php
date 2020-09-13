ca3494dc0b069733bfefb3b5504f1fee01d25217
Carsten Niehaus <ni@kgs-rastede.de>
2020-09-13 14:50:23 +0200

    Blog im div.container, damit es etwas schicker ist

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -3,28 +3,29 @@
 <?php snippet('page-header') ?>
 
 
-<div class="blockqoute">
+<div class="container">
+  <div class="blockqoute">
 
-  <div class="mt-3 blockquote-footer h5">
-    Datum: <?= $page->date()->toDate("d.m.Y") ?>
-    Autor: <?= $page->author() ?>
+    <div class="mt-3 blockquote-footer h5">
+      Datum: <?= $page->date()->toDate("d.m.Y") ?>
+      Autor: <?= $page->author() ?>
 
+    </div>
+
+    <?php snippet('tagliste', [
+      'item' => $page
+    ]) ?>
   </div>
 
-  <?php snippet('tagliste', [
-    'item' => $page
-  ]) ?>
+  <?= $page->text()->blocks() ?>
+
+  <?php if ($page->fotoansicht() == 'carousel') : ?>
+    <?php snippet('carousel') ?>
+  <?php elseif ($page->fotoansicht() == 'gallery') : ?>
+    <?php snippet('gallery') ?>
+  <?php else : ?>
+    <!-- Bilder werden vom Autor manuel gesetzt -->
+  <?php endif ?>
 </div>
-
-<?= $page->text()->blocks() ?>
-
-<?php if ($page->fotoansicht() == 'carousel') : ?>
-  <?php snippet('carousel') ?>
-<?php elseif ($page->fotoansicht() == 'gallery') : ?>
-  <?php snippet('gallery') ?>
-<?php else : ?>
-  <!-- Bilder werden vom Autor manuel gesetzt -->
-<?php endif ?>
-
 
 <?php snippet('footer') ?>
