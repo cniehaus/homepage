5913e65937a4376e356b76f6b18c9adfe7a515d5
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-04 22:55:45 +0200

    Anpassungen an die neue Datenstruktur

--- site/snippets/box-schnellzugriff.php
+++ site/snippets/box-schnellzugriff.php
@@ -1,16 +1,24 @@
 <div class="card mb-3 mt-3">
     <div class="card-body ">
         <h3>Zur Zeit aktuelle Downloads</h3>
-        <?php foreach (page('allgemeines/wichtigelinks')->inhalte()->toStructure() as $schnellverweis) : ?>
-            <?php if ($schnellverweis->startseite() == 'true') : ?>
+        <?php foreach (page('allgemeines/wichtigelinks')->Schule()->toFiles() as $schnellverweis) : ?>
+            <?php if ($schnellverweis->Startseite() == 'true') : ?>
                 <li class="list-group-item">
-                    <p>
-                        <?= $schnellverweis->beschreibung() ?>
-                    </p>
-                    <a class="btn btn-warning btn-lg btn-block" href="<?= $schnellverweis->link()->toFiles() ?>" role="button">
+                    <a class="btn btn-warning btn-lg btn-block" href="<?= $schnellverweis->url() ?>" role="button">
                         <svg class="bi" width="24" height="24">
                             <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#cloud-download" />
-                        </svg> <?= $schnellverweis->name() ?>
+                        </svg> <?= $schnellverweis->Anzeigename() ?>
+                    </a>
+                </li>
+            <?php endif ?>
+        <?php endforeach ?>
+        <?php foreach (page('allgemeines/wichtigelinks')->Unterricht()->toFiles() as $schnellverweis) : ?>
+            <?php if ($schnellverweis->Startseite() == 'true') : ?>
+                <li class="list-group-item">
+                    <a class="btn btn-warning btn-lg btn-block" href="<?= $schnellverweis->url() ?>" role="button">
+                        <svg class="bi" width="24" height="24">
+                            <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#cloud-download" />
+                        </svg> <?= $schnellverweis->Anzeigename() ?>
                     </a>
                 </li>
             <?php endif ?>
