2a12615a47a27a5d08a0b18fb7f7620cac38e101
NilsPvR <nilsundgrk@gmx.de>
2020-10-11 11:53:26 +0200

    Indikatoren nicht bei einem Bild zeigen

--- site/snippets/carousel.php
+++ site/snippets/carousel.php
@@ -7,8 +7,8 @@
         <?php if ($count == 0) : ?>
           <div class="carousel-item active">
           <?php else : ?>
-            <div class="carousel-item text-center">
-            <?php endif ?>
+          <div class="carousel-item text-center">
+          <?php endif ?>
 
             <a class="d-flex justify-content-center" href="<?= $image->url() ?>">
               <?= $image->resize(null, 600) ?>
@@ -17,25 +17,26 @@
             <div class="carousel-caption">
               <h4><?= $image->beschreibung() ?></h4>
             </div>
-            </div>
-            <?php endforeach ?>
           </div>
-      <a class="d-none d-sm-flex carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only text-dark">Zurück</span>
-      </a>
-      <a class="d-none d-sm-flex carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="sr-only text-dark">Weiter</span>
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      </a>
+          <?php endforeach ?>
+          </div>
+      <?php if ($page->gallery()->toFiles()->count >= 1) : ?>
+        <a class="d-none d-sm-flex carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only text-dark">Zurück</span>
+        </a>
+        <a class="d-none d-sm-flex carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="sr-only text-dark">Weiter</span>
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
 
-      <ol class="carousel-indicators">
-          <?php $count = -1;
-        foreach ($page->gallery()->toFiles() as $image) : $count++ ?>
-          <li data-target="#carouselExampleIndicators" <?php if ($count == 0) : ?> class="active" <?php endif ?> data-slide-to="<?= $count ?>"></li>
-        <?php endforeach ?>
-      </ol>
-
+        <ol class="carousel-indicators">
+            <?php $count = -1;
+          foreach ($page->gallery()->toFiles() as $image) : $count++ ?>
+            <li data-target="#carouselExampleIndicators" <?php if ($count == 0) : ?> class="active" <?php endif ?> data-slide-to="<?= $count ?>"></li>
+          <?php endforeach ?>
+        </ol>
+        <?php endif ?>
     </div>
   </div>
 </div>
