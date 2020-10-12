37ba5ceac4bc7301d4964a7040f075eac2486700
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-12 13:07:37 +0200

    Links im Winter nicht mehr aktuell

--- site/snippets/box-links.php
+++ site/snippets/box-links.php
@@ -1,20 +1,8 @@
 <div class="card mb-3 mt-3">
     <div class="card-body ">
         <ul>
-            <a href="https://nextcloud.kgs-rastede.logoip.de/index.php/login">
-                <img src="<?= $kirby->url('assets') ?>/bilder/nextcloud.svg" class="img-fluid">
-            </a>
-        </ul>
-        <hr>
-        <ul>
             <a href="https://thalia.webuntis.com/WebUntis/?school=kgs-rastede">
                 <img src="<?= $kirby->url('assets') ?>/bilder/webuntis.svg" class="img-fluid">
-            </a>
-        </ul>
-        <hr>
-        <ul>
-            <a href="https://moodle30.kgs-rastede.logoip.de/">
-                <img src="<?= $kirby->url('assets') ?>/bilder/moodle-kgs.svg" class="img-fluid">
             </a>
         </ul>
         <hr>
