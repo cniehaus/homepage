b1315815d198811448fc4e5dafe6b4deb6850a2d
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-12 21:11:39 +0200

    Die Fokusartikel sollen nun in der Seitenleiste sein

---
+++ site/snippets/box-fokus.php
@@ -0,0 +1,28 @@
+<div class="card mb-3 mt-3">
+
+    <!-- Jetzt folgen die als 'Topartikel' getaggte Blogs
+          Die tauchen hier immer auch, egal welches Datumsbereich
+          jeweils eingestellt ist
+         -->
+    <h1>Aktuell im Fokus</h1>
+    <div class="container-fluid">
+        <?php $index = 0;
+        foreach (page('blogs')
+            ->children()
+            ->flip() as $subpage) : $index++ ?>
+            <?php
+
+            // Ist es ein Topartikel?
+            if (
+                in_array("Topartikel", $subpage->tags()->split())
+
+                // ist der Artikel noch aktuell?
+                and ($subpage->datumStartseite()->toDate('Y-m-d-H-i-s') >= date('Y-m-d-H-i-s'))
+
+            ) {
+                snippet('topartikel', ['subpage' => $subpage]);
+            }
+            ?>
+        <?php endforeach ?>
+    </div>
+</div>
