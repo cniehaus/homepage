110584c7de502e0451cf4329ca22d037c778f69a
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-24 17:58:00 +0200

    Fokusliste luftiger und nun auch korrekte Links #55

--- site/snippets/box-fokus.php
+++ site/snippets/box-fokus.php
@@ -1,4 +1,4 @@
-<div class="card mb-3 mt-3">
+<div class="card mb-3 mt-5">
     <!-- Jetzt folgen die als 'Topartikel' getaggte Blogs -->
     <h1 class="ml-3">Aktuell im Fokus</h1>
     <div class="container-fluid">
@@ -18,16 +18,15 @@
 
             ) : ?>
 
-                <div class="container bg-danger">
-                    <h3 class="text-white">
-                        <?= $subpage->title() ?>
-                    </h3>
+                <li class="list-group-item">
+                    <a class="btn btn-warning btn-lg btn-block text-left" href="<?= $subpage->url() ?>" role="button">
+                        <h3>
+                            <?= $subpage->title() ?>
+                        </h3>
 
-                    <p class="text-white">
-                    <?= $subpage->Text()->blocks()->excerpt(150) ?>
-                        <a class="stretched-link" href="<?= $subpage->url() ?>">weiterlesen...</a>
-                    </p>
-                </div>
+                        <?= $subpage->Text()->blocks()->excerpt(150) ?>
+                    </a>
+                </li>
 
 
             <?php endif ?>
