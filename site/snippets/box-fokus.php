694a018b9e82806d42e8cd2d499d7993111b3797
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-24 11:03:46 +0200

    Fokusartikel deutlich gekürzt

--- site/snippets/box-fokus.php
+++ site/snippets/box-fokus.php
@@ -1,9 +1,5 @@
 <div class="card mb-3 mt-3">
-
-    <!-- Jetzt folgen die als 'Topartikel' getaggte Blogs
-          Die tauchen hier immer auch, egal welches Datumsbereich
-          jeweils eingestellt ist
-         -->
+    <!-- Jetzt folgen die als 'Topartikel' getaggte Blogs -->
     <h1 class="ml-3">Aktuell im Fokus</h1>
     <div class="container-fluid">
         <?php foreach (page('blogs')
@@ -13,15 +9,29 @@
 
             // Ist es ein Topartikel?
             if (
+                // handelt es sich um einen Artikel,
+                // auf der Startseite hervorzuheben ist?
                 in_array("Topartikel", $subpage->tags()->split())
 
                 // ist der Artikel noch aktuell?
                 and ($subpage->datumStartseite()->toDate('Y-m-d-H-i-s') >= date('Y-m-d-H-i-s'))
 
-            ) {
-                snippet('topartikel', ['subpage' => $subpage]);
-            }
-            ?>
+            ) : ?>
+
+                <div class="container bg-danger">
+                    <h3 class="text-white">
+                        <?= $subpage->title() ?>
+                    </h3>
+
+                    <p class="text-white">
+                    <?= $subpage->Text()->blocks()->excerpt(150) ?>
+                        <a class="stretched-link" href="<?= $subpage->url() ?>">weiterlesen...</a>
+                    </p>
+                </div>
+
+
+            <?php endif ?>
+
         <?php endforeach ?>
     </div>
 </div>
