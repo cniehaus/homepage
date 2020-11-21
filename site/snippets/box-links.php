f383acecffea23f35061a2f188058ee6f9e62139
Nils van Rijsinge <nilsundgrk@gmx.de>
2020-11-21 07:47:44 +0000

    Box-Links angepasst

--- site/snippets/box-links.php
+++ site/snippets/box-links.php
@@ -6,7 +6,6 @@ $links_bilder =
         'https://www.schuelerzeitung.kgs-rastede.de/' => 'irregulaer.png',
         'https://kgs-rastede.schulserver.de/iserv' => 'IServ_Logo_klein_RGB_clean.svg',
         'foerderverein/ueber_uns' => 'foerderverein.png',
-        'https://www.opc-asp.de/kgs-rastede/menuplan.php?VMP' => 'mensa.png'
     );
 $alt =
     array(
@@ -17,19 +16,27 @@ $alt =
         'Mensa'
     );
 
+$count = -1;
 ?>
 
-
-<div class="container">
-    <div class="row equal">
-        <?php foreach ($links_bilder as $link => $bild) : ?>
-            <div class="col-sm-4 d-flex pb-3">
-                <div class="card">
-                    <a href="<?= $link ?>"target="_blank">
-                        <img src="<?= $kirby->url('assets') ?>/bilder/<?= $bild ?>" class="img-fluid">
-                    </a>
+<div class="container-sm px-sm-5 px-md-0">
+    <div class="row row-cols-1 row-cols-md-5 g-2 g-xl-4 gx-sm-5 gx-md-2 mx-md-3">
+        <?php foreach ($links_bilder as $link => $bild) : $count++ ?>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body d-flex justify-content-center align-items-center">
+                        <a href="<?= $link ?>"><img src="<?= $kirby->url('assets') ?>/bilder/<?= $bild ?>" alt="<?= $alt[$count] ?>" class="img-fluid"></a>
+                    </div>
                 </div>
             </div>
         <?php endforeach ?>
+
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                    <a href="https://www.opc-asp.de/kgs-rastede/menuplan.php?VMP" target="_blank" class="btn btn-warning btn-lg h-100 w-100 text-nowrap d-flex justify-content-center align-items-center" role="button" aria-pressed="true"><h3>Mensa</h3></a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
