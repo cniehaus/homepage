0d3f5cfbed29e954f7076c08eb39d22dc6acd0e4
Nils van Rijsinge <nilsundgrk@gmx.de>
2020-11-21 15:22:18 +0000

    Resolve "YouTube Videos zu klein"

---
+++ site/snippets/editor/video.php
@@ -0,0 +1,9 @@
+<div class="col col-md-6">
+    <div class="ratio ratio-16x9">
+        <?= video($attrs->src()) ?>   
+    </div>
+</div>
+<!-- Das editor plugin verwendet diese Datei: https://github.com/getkirby/editor/wiki/Templates 
+    Ratio class: https://v5.getbootstrap.com/docs/5.0/helpers/ratio/
+    video() : https://getkirby.com/docs/reference/templates/helpers/video
+-->
