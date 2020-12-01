9c8d602ab77ccafadb0c3af8b4c558fa2c5f3051
NilsPvR <nilsundgrk@gmx.de>
2020-12-01 17:29:45 +0100

    Kommentare, kleine Anpassungen, "..." bei fehlenden Seiten

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -1,22 +1,23 @@
 <?php if ($pagination->hasPages()) : 
-$range = 4;
-$extraSeiten = 3;
+$range = 5; //Wird für die Länge der bzw. Anzahl der Elemente verwendet. Außerdem zur Berechnung der Extra Seiten: https://getkirby.com/docs/reference/objects/pagination/range
+$extraSeiten = 2; //Die Anzahl der Extraseiten. Also die ersten "paar" und die letzten "paar" Seiten
 $gesamtSeitenzahl = $pagination->pages();
 $aktuelleSeite = $pagination->page() ?>
     <nav>
         <ul class="pagination flex-wrap justify-content-center">
 
-            <?php if ($pagination->hasPrevPage()) : ?>
-             <li class="page-item">
+            <?php if ($pagination->hasPrevPage()) : //Wenn es eine Seite vor der aktuellen Seite gibt wird ein Doppelpfeil («) angezeigt, der zur vorherigen Seite führt?>
+                <li class="page-item">
                     <a class="page-link" href="<?= $pagination->prevPageURL() ?>">&laquo;</a>
-             </li>           
+                </li>           
             <?php endif ?>
 
             
             <?php for ($i = 1; $i <= $extraSeiten; $i++) : ?> 
-                <?php if ($i < ( $aktuelleSeite - ($range/2) + 0.5 )) : ?>
+                <?php if ($i < ( $aktuelleSeite - ($range/2) + 0.5 )) :
 
-                    <li class="page-item">
+                        //d-none d-sm-blok => auf dem Handy nicht anzeigen?>
+                    <li class="page-item d-none d-sm-block">
                         <a class="page-link" href="<?= $pagination->pageURL($i) ?>">
                             <?= $i ?>
                         </a>
@@ -25,18 +26,26 @@ $aktuelleSeite = $pagination->page() ?>
                 <?php endif ?>               
             <?php endfor ?>
 
-            <!-- Hier nun die mittleren Elemente -->
+            <?php if ($extraSeiten + 1 < $aktuelleSeite - ($range/2) +0.5 ) : 
+                //Es gibt Elemente zwischen der letzten ExtraSeite und den Seiten, die normal angzeigt werden. +0.5 siehe oben
+                
+                    //d-none d-sm-blok => auf dem Handy nicht anzeigen?>
+                <li class="page-item d-none d-sm-block">
+                    <a class="page-link">
+                        ...
+                    </a>
+                </li>
+            <?php endif ?>
 
-            <?php foreach ($pagination->range($range) as $r) : 
-            //Es folgen die ersten x Elemente der collection 
-            ?>
+
+            <!-- Hier nun die mittleren Elemente -->
+            <?php foreach ($pagination->range($range) as $r) : ?>                
                 
-                
-                    <li class="page-item <?= $aktuelleSeite === $r ? 'active' : '' ?>">
-                        <a class="page-link" <?= $aktuelleSeite === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
-                            <?= $r ?>
-                        </a>
-                    </li>
+                <li class="page-item <?= $aktuelleSeite === $r ? 'active' : '' ?>">
+                    <a class="page-link" <?= $aktuelleSeite === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
+                        <?= $r ?>
+                    </a>
+                </li>
                 
             <?php endforeach 
             /* Hier steckt hinter:
@@ -47,11 +56,23 @@ $aktuelleSeite = $pagination->page() ?>
             markiert wird */
             ?>
 
-            <?php 
-            for ($i = $gesamtSeitenzahl - ($extraSeiten-1); $i <= $gesamtSeitenzahl; $i++) : ?> 
-                <?php if ($i > ($aktuelleSeite + $range/2)) : ?>
 
-                    <li class="page-item">
+            <?php if ($gesamtSeitenzahl - $extraSeiten > $aktuelleSeite + ($range/2)) : 
+                //Es gibt Elemente zwischen der letzten normalen Seite und der ersten extra Seite
+
+                    //d-none d-sm-blok => auf dem Handy nicht anzeigen?>
+                <li class="page-item d-none d-sm-block">
+                    <a class="page-link">
+                        ...
+                    </a>
+                </li>
+            <?php endif ?>
+
+            <?php for ($i = $gesamtSeitenzahl - ($extraSeiten-1); $i <= $gesamtSeitenzahl; $i++) : ?> 
+                <?php if ($i > ($aktuelleSeite + $range/2)) : 
+                    
+                        //d-none d-sm-blok => auf dem Handy nicht anzeigen?>
+                    <li class="page-item d-none d-sm-block">
                         <a class="page-link" href="<?= $pagination->pageURL($i) ?>">
                             <?= $i ?>
                         </a>
@@ -60,7 +81,7 @@ $aktuelleSeite = $pagination->page() ?>
                 <?php endif ?>               
             <?php endfor ?>
 
-            <?php if ($pagination->hasNextPage()) : ?>
+            <?php if ($pagination->hasNextPage()) : //Wenn es eine Seite nach der aktuellen Seite gibt wird ein Doppelpfeil (») angezeigt, der zur nächsten Seite führt ?>
                 <li class="page-item">
                     <a class="page-link" href="<?= $pagination->nextPageURL() ?>">&raquo;</a>
                 </li>
