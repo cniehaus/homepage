2bf6fb17a5b019fdd41aa5ffea4f0000e44b8bea
Carsten Niehaus <ni@kgs-rastede.de>
2020-12-08 10:51:46 +0100

    Popper 2.x, Menü geht trotzdem nicht!

--- site/snippets/skripte-im-footer.php
+++ site/snippets/skripte-im-footer.php
@@ -1,2 +1,2 @@
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 <script src="<?= asset('assets/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
