fe3bfd81510ef8162354de8df2c286e0cba0e9aa
Nils van Rijsinge <nilsundgrk@gmx.de>
2020-12-08 09:12:46 +0000

    Resolve "Zitat-Formatierung im Editor fehlt komplett"

---
+++ site/snippets/editor/blockquote.php
@@ -0,0 +1,8 @@
+<div class="card bg-primary d-inline-flex mb-2 ml-5">
+    <div class="card-header p-2">
+        <p class="card-text fw-light fst-italic">
+            <?= $content ?>
+        </p>
+  </div>
+</div>
+<br>
