97872cb64610cffeebba8176b717c4c2a90c83e9
NilsPvR <nilsundgrk@gmx.de>
2020-12-14 17:03:31 +0100

    einmal schön machen

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,8 @@
 <?php snippet('header') ?>
 <?php snippet('home-page-header') ?>
 
+<?php snippet('box-notfall') ?>
 
-
-
-  <?php snippet('box-notfall') ?>
 <div class="container-fluid">
   <div class="row">  
     <div class="col-xl-8">
