5f8c7d14a572158b6e883387f880fe55029f39d9
NilsPvR <nilsundgrk@gmx.de>
2020-12-21 12:11:36 +0100

    Leitbild bs migration

--- site/templates/leitbild.php
+++ site/templates/leitbild.php
@@ -25,11 +25,11 @@
 
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading11">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse11" aria-expanded="false" aria-controls="flush-collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse11" aria-expanded="false" aria-controls="flush-collapseOne">
           Gemeinsames lernen
         </button>
       </h2>
-      <div id="flush-collapse11" class="accordion-collapse collapse" aria-labelledby="flush-heading11" data-parent="#accordionFlushExample">
+      <div id="flush-collapse11" class="accordion-collapse collapse" aria-labelledby="flush-heading11" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Die KGS Rastede ist eine Gesamtschule in der gemeinsames Lernen von Schülerinnen und Schülern von 5 -12/13 praktiziert wird und in der Schülerinnen und Schüler mit und ohne Behinderung gemeinsam erzogen und unterrichtet werden. Schulzweigunabhängige Kurse sollen die gegenseitige Achtung und Anerkennung unterschiedlicher Lern- und Interessensausprägungen fördern und den Schülerinnen und Schülern ermöglichen, die Vielfalt ihrer jeweiligen spezifischen Fähigkeiten in die Arbeit einer Gruppe einzubringen. Neben fachlichen werden gleichwertig soziale Lernziele verfolgt. Der gemeinsame Unterricht findet in den Jahrgängen 5-10 in den Fachbereichen Musisch-kulturelle Bildung, Sport, Arbeit-Wirtschaft und von 5-12/13 im Bereich der Arbeitsgemeinschaften statt.
           Um die Kontakte der Schülerinnen und Schüler der verschiedenen Schulzweige untereinander zu intensivieren, bilden –soweit es die Klassenverteilung zulässt - je eine Hauptschul-, eine Realschul- und eine Gymnasialklasse eines Jahrgangs ein ‚Haus‘. Dieses Haus stellt eine organisatorische Einheit dar, z.B. hinsichtlich der Zusammenstellung der Kurse im schulzweigübergreifenden Bereich, der Lage der Klassen im Gebäude, der Überlegungen für den Lehrereinsatz. </div>
@@ -38,11 +38,11 @@
 
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading10">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse10" aria-expanded="false" aria-controls="flush-collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse10" aria-expanded="false" aria-controls="flush-collapseOne">
           Individualisiertes Lernen nach Neigung und Interessen
         </button>
       </h2>
-      <div id="flush-collapse10" class="accordion-collapse collapse" aria-labelledby="flush-heading10" data-parent="#accordionFlushExample">
+      <div id="flush-collapse10" class="accordion-collapse collapse" aria-labelledby="flush-heading10" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Ein individualisiertes Lernangebot - neben einem verbindlichen Kanon allgemein bildender Grundlagen für alle - verstärkt die Lernfreude und damit den Lernerfolg und fördert spezifische Begabungen. Die KGS Rastede legt deshalb Wert auf die Arbeit in offenen, projektorientierten Unterrichtsformen und auf ein gefächertes Angebot an Wahlpflichtkursen und Arbeitsgemeinschaften. Viele Lehrerinnen und Lehrer beschreiten seit langem neue Wege des Lernens und lassen im Unterricht die Vielfalt des Lernens zu. Fördermaßnahmen in den Kernfächern und Hausaufgabenbetreuung sind Maßnahmen zur Kompensation von Defiziten. </div>
       </div>
@@ -50,12 +50,12 @@
 
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading9">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse9" aria-expanded="false" aria-controls="flush-collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse9" aria-expanded="false" aria-controls="flush-collapseOne">
           Interkulturelles Lernen – Lernen für Europa
           Schule ohne Rassismus-Schule mit Courage
         </button>
       </h2>
-      <div id="flush-collapse9" class="accordion-collapse collapse" aria-labelledby="flush-heading9" data-parent="#accordionFlushExample">
+      <div id="flush-collapse9" class="accordion-collapse collapse" aria-labelledby="flush-heading9" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Die Erziehung zu Toleranz und Verständigung und zur Akzeptanz anderer Kulturen ist Gegenstand der pädagogischen Arbeit unserer Schule. Dies umfasst sowohl die Auseinandersetzung mit den verschiedenen kulturellen Gegebenheiten vor Ort als auch Schulpartnerschaften und Schüleraustausch mit dem Ausland. Vor einigen Jahren sind wir als ‚Schule ohne Rassismus-Schule mit Courage‘ ausgezeichnet worden. Austauschvorhaben mit Frankreich, den Niederlanden, Polen, Spanien und Amerika haben ihren festen Platz an der Schule. Eine Schulpartnerschaft mit Nicaragua vermittelt Einblicke in das Leben südamerikanischer Kinder. Die Teilnahme an den Zertifikatsprüfungen Delf (Französisch), Cambridge (Englisch) und Dele (Spanisch) sollen weitere Lernanreize für die Entwicklung von Sprachenkompetenz schaffen.
         </div>
@@ -64,11 +64,11 @@
 
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-headingOne">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
           Umweltbewusstsein lernen und Bewegte Schule
         </button>
       </h2>
-      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
+      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Die Umweltbildung ist ein weiterer Schwerpunkt der Arbeit der KGS Rastede. Umwelterziehung wird in den Unterricht und das Schulleben wie auch in das tägliche Handeln aktiv einbezogen, die KGS Rastede bekennt sich daher zur Auszeichnung „Umweltschule“. Umweltbildung muss sich an den konkreten Lebenserfahrungen der Schülerinnen und Schüler orientieren. Dies setzt eine Öffnung der Schule und die Kooperation mit außerschulischen Fachleuten und Einrichtungen voraus. <br>Damit das Schulleben zur Sache der ganzen Schule wird und Identifikationsmöglichkeiten entstehen, ist die aktive Einbindung aller an der Schule Beteiligten, also Schülerinnen und Schüler, Eltern, Kollegium, andere Mitarbeiterinnen und Mitarbeiter und Schulträger nötig. Ehrenamtliches Engagement der Eltern innerhalb der Schule wird gefördert. Gesundheitserziehung schließt die vielfältigen Sportangebote einer bewegten Schule ein, die neben dem regulären Unterricht durch regelmäßige Sport- und Spielfeste und vielfältig sportlich ausgerichtete AG-Angebote ausgedrückt werden.
         </div>
@@ -76,11 +76,11 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-headingTwo">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
           Öffnung von Unterricht
         </button>
       </h2>
-      <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-parent="#accordionFlushExample">
+      <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Mit einer Öffnung des Unterrichts und des Schullebens zur Gemeinde sowie zu den Institutionen und Betrieben Rastedes bleibt die Gestaltung des Schulalltags lebensnah. Zudem rückt die KGS in das Bewusstsein der Menschen und wird integraler Bestandteil des Lebens am Ort. Dazu gehört, dass Bibliotheken, Einrichtungen und Ämter im Rahmen von Unterrichtsvorhaben erkundet und projektartig in die unterrichtliche Arbeit der Schule einbezogen werden. Ebenso wird mit Betrieben, mit der Jugendpflege, den Jugendhäusern in der Gemeinde, den Vereinen und Verbänden zusammengearbeitet.
         </div>
@@ -88,11 +88,11 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-headingThree">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseTwo">
           Praktisches Lernen und kulturelles Lernen
         </button>
       </h2>
-      <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-parent="#accordionFlushExample">
+      <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
 
           Lebensnahes Lernen bedeutet auch, dass Fragen der Berufsvorbereitung und Berufsorientierung die Landkarte des Lernens in der KGS Rastede bereichern. Schülerfirmen, Betriebserkundungen und Praktika in allen Schulzweigen sind pädagogische Veranstaltungen, um auf der Basis erworbenen ökonomischen Wissens ein besseres Verständnis betrieblicher Abläufe zu erlangen und persönliche Perspektiven nach der Schule vorzubereiten.
@@ -103,11 +103,11 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading4">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapseTwo">
           Informations- und kommunikationstechnologische Bildung
         </button>
       </h2>
-      <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-parent="#accordionFlushExample">
+      <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Das Konzept der KGS im Bereich der Informations- und kommunikationstechnologischen Bildung versucht den Anforderungen der Medien- und Berufswelt und den Gefahren, die im Netz vorhanden sind, gerecht zu werden. Es umfasst die Jahrgänge 5-12. Schülerinnen und Schüler erlernen aufbauend den Umgang mit dem PC, der Computer soll als Werkzeug der Informationsgewinnung benutzt werden, um die schulische Allgemeinbildung zu verbessern. Aufklärung über Gefahren im Netz ergänzen das Angebot.
         </div>
@@ -115,10 +115,10 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading5">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapseTwo">
           Stärken stärken </button>
       </h2>
-      <div id="flush-collapse5" class="accordion-collapse collapse" aria-labelledby="flush-heading5" data-parent="#accordionFlushExample">
+      <div id="flush-collapse5" class="accordion-collapse collapse" aria-labelledby="flush-heading5" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Schule leistet immer auch einen erheblichen Beitrag zur Erziehung von Kindern und Jugendlichen. Klassenlehrerinnen und Klassenlehrer in den 5. und 6. Klassen setzen ein Erziehungscurriculum (Erwachsen Werden) um, das Kinder präventiv vor Sinn- und Lebenskrisen schützen soll. Ergänzt werden diese Maßnahmen durch „Schüler-Mediatoren“ und andere Beratung und Erziehung begleitende Projekte sowie durch Schulsozialarbeit im Hauptschulzweig.
 
@@ -127,10 +127,10 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading6">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapseTwo">
           Ganztägig lernen </button>
       </h2>
-      <div id="flush-collapse6" class="accordion-collapse collapse" aria-labelledby="flush-heading6" data-parent="#accordionFlushExample">
+      <div id="flush-collapse6" class="accordion-collapse collapse" aria-labelledby="flush-heading6" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Die KGS Rastede ist eine offene Ganztagsschule, die allen Schülerinnen und Schülern an vier Nachmittagen in der Woche ein Angebot unterbreitet. Dieses erweiterte Bildungsangebot schafft Möglichkeiten der Vertiefung der individuellen Persönlichkeits-, Lern- und Leistungsentwicklung. Das verstehen wir als einen Beitrag zur Qualitätssteigerung schulischer Arbeit an der KGS Rastede durch erweiterte Bildungs- und Fördermöglichkeiten für hochbegabte und leistungsschwächere Schülerinnen und Schüler.
         </div>
@@ -138,10 +138,10 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading7">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse7" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse7" aria-expanded="false" aria-controls="flush-collapseTwo">
           Transparenz und demokratisches Miteinander </button>
       </h2>
-      <div id="flush-collapse7" class="accordion-collapse collapse" aria-labelledby="flush-heading7" data-parent="#accordionFlushExample">
+      <div id="flush-collapse7" class="accordion-collapse collapse" aria-labelledby="flush-heading7" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Das Leitbild und die Profile der KGS erweitern die Aufgabenstellung für Lehrerinnen und Lehrer. Neben schulzweigspezifischen Angelegenheiten müssen Fragen der alle Schulzweige umfassenden Jahrgangsstufen berücksichtigt und beantwortet werden. Dieser Prozess findet in gemeinsamen Fach- und Fachbereichskonferenzen, Gesamtkonferenzen und pädagogischen Konferenzen mit Eltern und Schülern statt. Die kollegiale Schulleitung übernimmt Aufgaben, die die gesamte Schule betreffen.
         </div>
@@ -149,10 +149,10 @@
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading8">
-        <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapse8" aria-expanded="false" aria-controls="flush-collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse8" aria-expanded="false" aria-controls="flush-collapseTwo">
           Evaluation schulischer Arbeit </button>
       </h2>
-      <div id="flush-collapse8" class="accordion-collapse collapse" aria-labelledby="flush-heading8" data-parent="#accordionFlushExample">
+      <div id="flush-collapse8" class="accordion-collapse collapse" aria-labelledby="flush-heading8" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           Es gehört zum Anspruch der Schule, die Qualität schulischer Arbeit ständig zu verbessern. Aus diesem Grund überprüfen und bewerten alle an Schule Beteiligten kontinuierlich den Erfolg der Arbeit, indem alle relevanten Aspekte von Qualität einer Evaluation unterzogen werden können. Zu den Instrumenten des Qualitätsmanagements gehören programmgestützte Verfahren, Interviews, Leistungsvergleiche und die Bewertung schulischer Daten.
         </div>
