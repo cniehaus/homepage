e19d767de0ed249770129fabcbd70c00635b8132
Carsten Niehaus <ni@kgs-rastede.de>
2020-12-31 18:25:56 +0100

    Doku dokumentieren

--- site/controllers/search.php
+++ site/controllers/search.php
@@ -1,9 +1,26 @@
 <?php
 
+// Anleitung dazu hier: 
+// https://getkirby.com/docs/cookbook/content/search
+
 return function ($site) {
 
   $query   = get('q');
-  $sidewide = pages(["blogs", "kontakte", "Faecher", "allgemeines", "foerderverein", "formulare", "schule", "ser", "sv", "unterricht"])->children()->published();
+  $sidewide = pages([
+    "blogs",
+    "kontakte",
+    "schulleitung",
+    "2_ni",
+    "Faecher",
+    "allgemeines",
+    "foerderverein",
+    "formulare",
+    "schule",
+    "ser",
+    "sv",
+    "unterricht"
+  ])
+    ->children()->published();
   $results = $sidewide->search($query, 'title|text');
   $results = $results->paginate(10);
 
@@ -12,5 +29,4 @@ return function ($site) {
     'results'    => $results,
     'pagination' => $results->pagination()
   ];
-
 };
