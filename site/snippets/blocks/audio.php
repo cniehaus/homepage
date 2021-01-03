d0a170d123c2c7e1fd0970aba01f46eafc197da1
Carsten Niehaus <ni@kgs-rastede.de>
2021-01-03 12:37:13 +0100

    Unterstützung von Audio-Blocks

---
+++ site/snippets/blocks/audio.php
@@ -0,0 +1,6 @@
+<div class="container">
+  <audio controls>
+    <source src="<?= $block->soundfile()->toFile() ?>" type="audio/mpeg">
+    Your browser does not support the audio element.
+  </audio>
+</div>
