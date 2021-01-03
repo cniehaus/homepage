7485b76a27a318ece49a148b10616a0450411eb3
Carsten Niehaus <ni@kgs-rastede.de>
2021-01-03 12:59:49 +0100

    Design angepasst

--- site/snippets/blocks/audio.php
+++ site/snippets/blocks/audio.php
@@ -1,6 +1,12 @@
+<!-- 
+  site/blueprints/blocks/audio.php
+ -->
 <div class="container">
   <audio controls>
     <source src="<?= $block->soundfile()->toFile() ?>" type="audio/mpeg">
     Your browser does not support the audio element.
   </audio>
+  <div class="blockquote-footer p-3 ">
+     <cite title="Source Title"><?= $block->name() ?></cite>
+  </div>
 </div>
