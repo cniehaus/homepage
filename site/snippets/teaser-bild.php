4abe2ba9be7f5e14c58360729894ae188c7ef4e8
Carsten Niehaus <ni@kgs-rastede.de>
2021-03-10 18:03:11 +0100

    können wohl weg...

--- site/snippets/teaser-bild.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-4">
-    
-    <?php if ($subpage->hasImages() > 0) : ?>
-        <img src="<?= $subpage->images()->first()->url() ?>" class="card-img-top rounded" alt="<?= $subpage->images()->first()->alt() ?>">
-    <?php else : ?>
-    <?php endif ?>
-
-    
-</div>
