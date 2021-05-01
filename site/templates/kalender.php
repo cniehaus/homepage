56022be2b81ab8ec4c57b8e1282c77c46e365958
Carsten Niehaus <ni@kgs-rastede.de>
2021-05-01 19:43:18 +0200

    einen Schritt weiter

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -6,13 +6,10 @@
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.2/css/all.css">
 */ ?>
 
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar/main.min.js'></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar/main.min.css" />
 <script src='https://github.com/mozilla-comm/ical.js/releases/download/v1.4.0/ical.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@5.6.0/main.global.js'></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar/main.min.css" />
-
-
-
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar/main.min.js'></script>
 
 
 <script>
@@ -26,45 +23,46 @@
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
+
       buttonText: {
         today: 'Heute',
         month: 'Monat',
         week: 'Woche',
         day: 'Tag',
       },
+
       // Samstag und Sonntag verstecken
-      hiddenDays: [0, 6],
+      hiddenDays: [0, 6], 
       firstDay: 1,
       navLinks: true, // can click day/week names to navigate views
       editable: false,
-      handleWindowResize: true,
+      handleWindowResize: true, 
 
       //Zeigt eine rote Linie an, die die aktuelle Zeit darstellt
-      nowIndicator: true,
+      nowIndicator: true, 
 
       //Die Stunden von 20 Uhr bis 07 Uhr morgens müssen nicht
       //angezeigt werden, dahier nie schulische Veranstaltungen sind
-      slotMinTime: "07:00:00",
-      slotMaxTime: "20:00:00",
+      slotMinTime: "07:00:00",  
+      slotMaxTime: "20:00:00",  
 
       // Kalenderwoche anzeigen
-      weekNumbers: true,
+      weekNumbers: true, 
 
       allDayText: "ganztägig",
-
+      
       noEventsContent: 'Keine Ereignisse anzuzeigen',
-
+      
       displayEventTime: false, // don't show the time column in list view
+    // Die Datei laden. Aktuell muss diese Datei noch per Hand generiert werden
       events: {
-        /*url: 'https://kgs-rastede.eu/iserv/public/calendar/?key=1bad7fec91a3d07904543f4476774fd1',*/
-        /*url: 'https://kgs-rastede.de/assets/kalender/public.ics',*/
-        /*url: 'ics/public.ics',*/
-        url: 'https://www.kip-ol.de/_kalender/_fullcalendar/examples/ics/public.ics',
-        format: 'ics',
+		  /*url: 'https://kgs-rastede.eu/iserv/public/calendar/?key=1bad7fec91a3d07904543f4476774fd1',*/
+		  /*url: '<?= $kirby->url('assets') ?>/kalender/public.ics',*/
+		  url: 'https://kgs-rastede.de/assets/kalender/public.ics',
+		  format: 'ics',
         failure: function() {
           document.getElementById('script-warning').style.display = 'block';
-        }
-      },
+		  },
       loading: function(bool) {
         document.getElementById('loading').style.display =
           bool ? 'block' : 'none';
@@ -72,9 +70,19 @@
     });
 
     calendar.render();
+    calendar.refetchEvents(); //Test
   });
 </script>
+<div class="container-fluid">
+  <div id='calendar'></div>
+</div>
+<div id='script-warning'>
+    <code>public.ics</code> konnte nicht geladen werden
 
-<div id='calendar'></div>
+  </div>
+
+  <div id='loading'>loading...</div>
+
+  <div id='calendar'></div>
 
 <?php snippet('footer') ?>
