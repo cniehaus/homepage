8bbdca557e909a06bc228b042061fed8f96c84c6
Carsten Niehaus <ni@kgs-rastede.de>
2021-05-02 12:49:51 +0200

    dieses Warnung unten will ich nicht haben.

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -30,32 +30,32 @@
       },
 
       // Samstag und Sonntag verstecken
-      hiddenDays: [0, 6], 
+      hiddenDays: [0, 6],
       firstDay: 1,
       navLinks: true, // can click day/week names to navigate views
       editable: false,
-      handleWindowResize: true, 
+      handleWindowResize: true,
 
       //Zeigt eine rote Linie an, die die aktuelle Zeit darstellt
-      nowIndicator: true, 
+      nowIndicator: true,
 
       //Die Stunden von 20 Uhr bis 07 Uhr morgens müssen nicht
       //angezeigt werden, dahier nie schulische Veranstaltungen sind
-      slotMinTime: "07:00:00",  
-      slotMaxTime: "20:00:00",  
+      slotMinTime: "07:00:00",
+      slotMaxTime: "20:00:00",
 
       // Kalenderwoche anzeigen
-      weekNumbers: true, 
+      weekNumbers: true,
 
       allDayText: "ganztägig",
-      
+
       noEventsContent: 'Keine Ereignisse anzuzeigen',
-      
+
       displayEventTime: false, // don't show the time column in list view
-      
+
       events: {
-		  /*url: 'https://kgs-rastede.eu/iserv/public/calendar/?key=1bad7fec91a3d07904543f4476774fd1',*/
-		  url: '<?= $kirby->url('assets') ?>/kalender/public.ics',
+        /*url: 'https://kgs-rastede.eu/iserv/public/calendar/?key=1bad7fec91a3d07904543f4476774fd1',*/
+        url: '<?= $kirby->url('assets') ?>/kalender/public.ics',
         format: 'ics',
         failure: function() {
           document.getElementById('script-warning').style.display = 'block';
@@ -74,13 +74,12 @@
 <div class="container-fluid">
   <div id='calendar'></div>
 </div>
-<div id='script-warning'>
-    <code>public.ics</code> konnte nicht geladen werden
+<!-- <div id='script-warning'>
+  <code>public.ics</code> konnte nicht geladen werden
+</div>
 
-  </div>
+<div id='loading'>loading...</div>
 
-  <div id='loading'>loading...</div>
-
-  <div id='calendar'></div>
+<div id='calendar'></div> -->
 
 <?php snippet('footer') ?>
