686f0d6e7a23eea45b7aac6ceaeaba16e2892376
Carsten Niehaus <ni@kgs-rastede.de>
2021-05-05 09:28:18 +0200

    template für das Herausforderungsprojekt

---
+++ site/templates/herausforderungsprojekt.php
@@ -0,0 +1,24 @@
+<?php snippet('header') ?>
+<?php snippet('page-header') ?>
+
+<?php snippet('sidebar') ?>
+
+<div class="container">
+
+  <h2>Berichte aus dem Herausforderungsprojekt</h2>
+
+  <?php
+  foreach (page('blogs')
+    ->children()
+    ->listed()
+    ->filterBy('tags', 'Herausforderung', ',')
+    ->flip() as $subpage) :
+
+    snippet('blogkarte', ['subpage' => $subpage]);
+  ?>
+
+  <?php endforeach ?>
+
+</div>
+
+<?php snippet('footer') ?>
