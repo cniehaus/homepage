01fa4f5250f391a4d4efb7db05df76d2d78084a2
Carsten Niehaus <ni@kgs-rastede.de>
2021-05-31 21:30:14 +0200

    container, damit es gleichförmig ist

--- site/templates/nutzerordnung.php
+++ site/templates/nutzerordnung.php
@@ -1,26 +1,30 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-<p>
-    <?= $page->Beschreibung()->kirbytext() ?>
-</p>
 
-<div class="row row-cols-1 row-cols-md-2 g-4">
-    <?php
-    $dateien =  $page->dateien()->toFiles();
-    foreach ($dateien as $datei) : ?>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $datei->anzeigename()->or($datei->name()) ?></h5>
-                    <p class="card-text"><?= $datei->beschreibung() ?></p>
-                    <a href="<?= $datei->url() ?>" class="btn btn-secondary mb-5" role="button">Datei herunterladen</a>
 
+<div class="container">
+    <p>
+        <?= $page->Beschreibung()->kirbytext() ?>
+    </p>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <?php
+        $dateien =  $page->dateien()->toFiles();
+        foreach ($dateien as $datei) : ?>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $datei->anzeigename()->or($datei->name()) ?></h5>
+                        <p class="card-text"><?= $datei->beschreibung() ?></p>
+                        <a href="<?= $datei->url() ?>" class="btn btn-secondary mb-5" role="button">Datei herunterladen</a>
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-    <?php endforeach ?>
+        <?php endforeach ?>
+    </div>
+
 </div>
 
 <?php snippet('sidebar') ?>
