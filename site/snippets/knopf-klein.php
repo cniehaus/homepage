238f97b124f6588ccd3887a52a3f8c951d90b195
Carsten Niehaus <ni@kgs-rastede.eu>
2021-09-18 19:34:16 +0200

    dieses Design wurde gewünscht

--- site/snippets/knopf-klein.php
+++ site/snippets/knopf-klein.php
@@ -3,7 +3,7 @@
 Layout überall konsistent ist-->
 
 <a 
-class="btn-sm btn-primary text-decoration-none stretched-link" 
+class="btn-sm text-decoration-none stretched-link" 
 role="button" 
 href="<?= $subpage->url() ?>" 
 aria-disabled="false"
