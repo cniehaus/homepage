1f86431cb7bd58cd2edfeebf9b08a90671b5fc78
Carsten Niehaus <ni@kgs-rastede.eu>
2021-10-26 15:47:35 +0200

    Netter Zoomeffekt.

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -4,11 +4,16 @@
 
 
 <div class="row" data-masonry='{"percentPosition": true }'>
-
     <?php foreach ($block->images()->toFiles() as $image) : ?>
         <div class="col-sm-6 col-lg-4 mb-4">
-            <img src="<?= $image->url() ?>" class="rounded img-fluid" alt="<?= $image->beschreibung() ?>">
+            <div class="ohnezoom" style="overflow:hidden;">
+                <img class="rounded hover img-fluid" src="<?= $image->url() ?>"   >
+                <div class="mitzoom">
+                    <h2 style="text-align:center;margin-top:-6px"><?= $image->beschreibung() ?></h2>
+                </div>
+            </div>
         </div>
+
     <?php endforeach ?>
 </div>
 
