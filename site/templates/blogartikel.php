4fe49163399c4fd7ee0c63a7cfd66f60c20d69dc
Carsten Niehaus <ni@kgs-rastede.eu>
2021-12-24 10:04:22 +0100

    neuer, moderner Artikeltyp

---
+++ site/templates/blogartikel.php
@@ -0,0 +1,74 @@
+<?php snippet('header') ?>
+
+<?php snippet('page-header') ?>
+
+
+<div class="container">
+  <div class="blockqoute">
+    <div class="d-flex justify-content-between mb-3">
+
+      <?php if ($page->date()->isNotEmpty() || $page->author()->isNotEmpty()) : ?>
+        <div class="mt-0 mb-0 h5">
+
+          <?php if ($page->date()->isNotEmpty()) : ?>
+            <?= $page->date()->toDate("d.m.Y") ?>,
+          <?php endif ?>
+          <?php if ($page->author()->isNotEmpty()) : ?>
+            geschrieben von: <?= $page->author() ?>
+          <?php endif ?>
+
+        </div>
+
+      <?php else : ?>
+        <div></div>
+      <?php endif ?>
+
+
+
+
+      <?php snippet('tagliste', [
+        'item' => $page
+      ]) ?>
+    </div>
+  </div>
+</div>
+
+
+<div class="col">
+      One of three columns
+    </div>
+  
+
+
+
+
+
+<?php foreach ($page->layout()->toLayouts() as $layout) : ?>
+  <section class="grid" id="<?= $layout->id() ?>">
+  <div class="row align-items-start">
+    <?php foreach ($layout->columns() as $column) : ?>
+      <div class="col">
+        <div class="blocks">
+          <?= $column->blocks() ?>
+        </div>
+      </div>
+    <?php endforeach ?>
+  </div>
+  </section>
+<?php endforeach ?>
+
+
+
+<div class="container">
+
+  <?php if ($page->fotoansicht() == 'carousel') : ?>
+    <?php snippet('carousel') ?>
+  <?php elseif ($page->fotoansicht() == 'gallery') : ?>
+    <?php snippet('gallery') ?>
+  <?php else : ?>
+    <!-- Bilder werden vom Autor manuel gesetzt -->
+  <?php endif ?>
+
+</div>
+
+<?php snippet('footer') ?>
