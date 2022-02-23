478599e15f25d646bd590ba6b7237b8a225fb474
NilsPvR <nilsundgrk@gmx.de>
2022-02-23 13:24:14 +0100

    2x Elemente richtig einrücken und falsches div weg

--- site/snippets/blocks/karousel.php
+++ site/snippets/blocks/karousel.php
@@ -9,9 +9,9 @@
           foreach ($block->karousel()->toFiles() as $image) : $count++ ?>
             <?php if ($count == 0) : ?>
               <div class="carousel-item active">
-              <?php else : ?>
-                <div class="carousel-item">
-                <?php endif ?>
+            <?php else : ?>
+              <div class="carousel-item">
+            <?php endif ?>
 
                 <a class="d-flex justify-content-center" href="<?= $image->url() ?>">
                   <img class="d-block mw-100 img img-raised rounded" alt="<?= $image->alt() ?>" src="<?= $image->resize(null, 600)->url() ?>">
@@ -23,37 +23,31 @@
                   </div>
                 </div>
 
-                </div>
-              <?php endforeach ?>
-
               </div>
-
-              <?php if ($block->karousel()->toFiles()->count() >= 2) : ?>
-                <a class="d-none d-sm-flex carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only text-dark">Zurück</span>
-                </a>
-                <a class="d-none d-sm-flex carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                  <span class="sr-only text-dark">Weiter</span>
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                </a>
-
-                <ol class="carousel-indicators">
-                  <?php $count = -1;
-                  foreach ($block->karousel()->toFiles() as $image) : $count++ ?>
-                    <li data-bs-target="#carouselExampleIndicators" <?php if ($count == 0) : ?> class="active" <?php endif ?> data-bs-slide-to="<?= $count ?>"></li>
-                  <?php endforeach ?>
-                </ol>
-              <?php endif ?>
+          <?php endforeach ?>
 
         </div>
+
+        <?php if ($block->karousel()->toFiles()->count() >= 2) : ?>
+          <a class="d-none d-sm-flex carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only text-dark">Zurück</span>
+          </a>
+          <a class="d-none d-sm-flex carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+            <span class="sr-only text-dark">Weiter</span>
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          </a>
+
+          <ol class="carousel-indicators">
+            <?php $count = -1;
+            foreach ($block->karousel()->toFiles() as $image) : $count++ ?>
+              <li data-bs-target="#carouselExampleIndicators" <?php if ($count == 0) : ?> class="active" <?php endif ?> data-bs-slide-to="<?= $count ?>"></li>
+            <?php endforeach ?>
+          </ol>
+        <?php endif ?>
+
+
       </div>
-
-
-
-
-
-
     </div>
   </div>
 <?php endif ?>
--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -3,33 +3,33 @@
   if ($image = $page->hintergrundbild()->toFile()) : ?>
 
     <div class="p-5 text-center bg-image" style="
-            background-image: url('<?= $image->url() ?>');
-            background-repeat: no-repeat;
-            background-size: cover;
-            height:30vh">
+      background-image: url('<?= $image->url() ?>');
+      background-repeat: no-repeat;
+      background-size: cover;
+      height:30vh">
 
-    <?php else : ?>
-      <div class="p-5 text-center bg-image" style="
-    background-image: url('<?= $kirby->url('assets') ?>/img/IMG_1343.jpeg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    height:30vh">
-      <?php endif ?>
-      </div>
+  <?php else : ?>
+    <div class="p-5 text-center bg-image" style="
+      background-image: url('<?= $kirby->url('assets') ?>/img/IMG_1343.jpeg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      height:30vh">
+  <?php endif ?>
 
-
-
-
-      <!-- Titelfeld -->
-
-      <div class="p-3 mb-2 bg-light text-secondary">
-        <h1>
-          <?= $page->title() ?>
-        </h1>
-      </div>
-      <small class="text-muted"><?= $page->subtitle() ?></small>
-
-      <h1 class="display-6 mb-3 ms-3">
-        <?= $page->heading() ?>
-      </h1>
     </div>
+
+
+
+
+  <!-- Titelfeld -->
+
+  <div class="p-3 mb-2 bg-light text-secondary">
+    <h1>
+      <?= $page->title() ?>
+    </h1>
+  </div>
+  <small class="text-muted"><?= $page->subtitle() ?></small>
+
+  <h1 class="display-6 mb-3 ms-3">
+    <?= $page->heading() ?>
+  </h1>
