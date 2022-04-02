975bbc61eba33c9be201147feb49752c51fdf96e
Carsten Niehaus <gitlab@niehaus-ol.de>
2022-04-02 11:17:31 +0200

    IServ hat vor ein paar Wochen das Logo gewechselt, hier nun auch den Wechsel vollzogen

--- site/snippets/box-links.php
+++ site/snippets/box-links.php
@@ -4,7 +4,7 @@ $links_bilder =
     array(
         'https://thalia.webuntis.com/WebUntis/?school=kgs-rastede' => 'webuntis.png',
         'https://www.schuelerzeitung.kgs-rastede.de/' => 'irregulaer.png',
-        'https://kgs-rastede.eu' => 'IServ_Logo_klein_RGB_clean.svg',
+        'https://kgs-rastede.eu' => 'IServ_Logo.svg',
         'foerderverein' => 'foerderverein.png',
         'https://www.opc-asp.de/kgs-rastede/menuplan.php?VMP' => 'mensa.png',
     );
