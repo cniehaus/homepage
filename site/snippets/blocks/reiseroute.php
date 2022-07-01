a73457609ab2c58db7a27e84d32a2aa266e9a14c
Carsten Niehaus <gitlab@niehaus-ol.de>
2022-07-01 15:22:46 +0200

    neue Version

--- site/snippets/blocks/reiseroute.php
+++ site/snippets/blocks/reiseroute.php
@@ -1,5 +1,6 @@
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js"></script>
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
+
 <style>
   body {
     margin: 0;
