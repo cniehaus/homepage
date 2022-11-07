797e0723d50ffbb6dec1e67c2d8c7ab789a01e48
Carsten Niehaus <gitlab@niehaus-ol.de>
2022-11-07 16:32:31 +0100

    Werden wir so nie verwenden, kann/muss weg

--- site/templates/emails/iserv.html.php
+++
@@ -1,7 +0,0 @@
-<p>[Diese E-Mail wurde über die Schulhomepage versendet]</p>
-
-<p>Folgende/-r Schüler/-in möchte ein neues IServ-Passwort beantragen:</p>
-
-</p><?= $text ?></p>
-
-<p>Mit freundlichen Grüßen von<br><?= $sender ?></p>
