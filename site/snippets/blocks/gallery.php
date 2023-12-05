6caad102ced7f09f94f9cd32aaf971ac1147bdf8
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-05 22:17:38 +0100

    angepasst an tailwindcss

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -3,18 +3,31 @@
 /** @var \Kirby\Cms\Block $block */ ?>
 
 
-<div class="row" data-masonry='{"percentPosition": true }'>
-    <?php foreach ($block->images()->toFiles() as $image) : ?>
-        <div class="col-sm-6 col-lg-4 mb-4">
-            <div class="ohnezoom" style="overflow:hidden;">
-                <img class="rounded hover img-fluid" src="<?= $image->url() ?>"   >
-                <div class="mitzoom">
-                    <h2 style="text-align:center;margin-top:-6px"><?= $image->beschreibung() ?></h2>
+
+
+
+<!-- Image Overlays Section: Image Zoom In -->
+<div class="bg-white dark:bg-gray-900 dark:text-gray-100">
+    <div class="container mx-auto space-y-16 px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
+
+            <?php foreach ($block->images()->toFiles() as $image) : ?>
+
+                <div class="group relative overflow-hidden rounded-lg bg-indigo-900 focus-within:ring-4 focus-within:ring-indigo-500 focus-within:ring-opacity-50 focus-within:ring-offset-2 focus:outline-none focus:ring-4 focus:ring-indigo-500 focus:ring-opacity-50 focus:ring-offset-2 dark:ring-offset-gray-900" tabindex="0">
+                    <img src="<?= $image->url() ?>" class="transition duration-300 ease-out group-focus-within:scale-125 group-hover:scale-125 group-focus:scale-125" alt="Image 1" />
+
+                    <!-- Item Overlay -->
+                    <div class="absolute inset-0 flex flex-col items-center justify-center rounded bg-slate-900 bg-opacity-80 opacity-0 transition duration-300 ease-out group-focus-within:opacity-100 group-hover:opacity-100 group-focus:opacity-100">
+                        <div class="text-center">
+                            <h4 class="text-lg font-semibold text-white"><?= $image->beschreibung() ?></h4>
+                        </div>
+                    </div>
+                    <!-- END Item Overlay -->
                 </div>
-            </div>
+
+            <?php endforeach ?>
+
         </div>
-
-    <?php endforeach ?>
+    </div>
 </div>
-
-<?= js('assets/js/load_masonry.js') ?>
+<!-- END Image Overlays Section: Image Zoom In -->
--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -9,35 +9,34 @@ $ratio   = $block->ratio()->or('auto');
 $src     = null;
 
 if ($block->location() == 'web') {
-    $src = $block->src()->esc();
+  $src = $block->src()->esc();
 } elseif ($image = $block->image()->toFile()) {
-    $alt = $alt ?? $image->alt();
-    $src = $image->url();
+  $alt = $alt ?? $image->alt();
+  $src = $image->url();
 }
 
 /**
  * Erweiterung, um Bilder links/rechts/mittig darzustellen
  * https://getbootstrap.com/docs/5.3/content/images/#responsive-images
  */
-$orientation = $block->orientation() == 'links' ? 'float-start' :
-       ($block->orientation() == 'rechts' ? 'float-end' :
-       ($block->orientation() == 'mitte' ? 'mx-auto d-block' : ''));
+$orientation = $block->orientation() == 'links' ? 'float-start' : ($block->orientation() == 'rechts' ? 'float-end' : ($block->orientation() == 'mitte' ? 'mx-auto d-block' : ''));
 ?>
 
-<?php if ($src): ?>
-<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
-  <?php if ($link->isNotEmpty()): ?>
-  <a href="<?= Str::esc($link->toUrl()) ?>">
-    <img src="<?= $src ?>" class="<?= $orientation ?>" alt="<?= $alt->esc() ?>">
-  </a>
-  <?php else: ?>
-  <img src="<?= $src ?>" class="<?= $orientation ?>" alt="<?= $alt->esc() ?>">
-  <?php endif ?>
 
-  <?php if ($caption->isNotEmpty()): ?>
-  <figcaption>
-    <?= $caption ?>
-  </figcaption>
-  <?php endif ?>
-</figure>
+<?php if ($src) : ?>
+  <figure <?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+    <?php if ($link->isNotEmpty()) : ?>
+      <a href="<?= Str::esc($link->toUrl()) ?>">
+        <img src="<?= $src ?>" class="h-auto my-3 max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 <?= $orientation ?>" alt="<?= $alt->esc() ?>">
+      </a>
+    <?php else : ?>
+      <img src="<?= $src ?>" class="h-auto my-3 max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 <?= $orientation ?>" alt="<?= $alt->esc() ?>">
+    <?php endif ?>
+
+    <?php if ($caption->isNotEmpty()) : ?>
+      <figcaption>
+        <?= $caption ?>
+      </figcaption>
+    <?php endif ?>
+  </figure>
 <?php endif ?>
