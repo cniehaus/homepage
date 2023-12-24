f025e75e19844ae45abaa01c299ef9b9c959baac
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-24 11:12:33 +0100

    Ein div war überflüssig

--- site/templates/faecher.php
+++ site/templates/faecher.php
@@ -4,7 +4,7 @@
 
 <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 gx-4">
 
-  <?php foreach ($page->children() as $fb) : ?>
+  <?php foreach ($page->children() as $fb): ?>
     <div class="card mb-3">
 
 
@@ -12,22 +12,28 @@
 
       <div class="card-body">
         <?php
-        $images =  $fb->symbolbild()->toFiles();
-        foreach ($images as $image) : ?>
+        $images = $fb->symbolbild()->toFiles();
+        foreach ($images as $image): ?>
           <img src="<?= $image->url() ?>" alt="">
         <?php endforeach ?>
 
 
 
-        <h3 class="card-title mb-3 text-warning"><?= $fb->title() ?></h3>
-        <h3 class="card-title"><?= $fb->bezeichnung() ?>:</h3>
-        <h3> <?= $fb->namefbl() ?></h3>
+        <h3 class="card-title mb-3 text-warning">
+          <?= $fb->title() ?>
+        </h3>
+        <h3 class="card-title">
+          <?= $fb->bezeichnung() ?>:
+        </h3>
+        <h3>
+          <?= $fb->namefbl() ?>
+        </h3>
         <div class="list-group text-white">
 
 
           <?php
-          $relatedPages =  $fb->pages()->toPages();
-          foreach ($relatedPages as $relatedPage) : ?>
+          $relatedPages = $fb->pages()->toPages();
+          foreach ($relatedPages as $relatedPage): ?>
 
             <a href="<?= $relatedPage->url() ?>" class="list-group-item list-group-item-light list-group-item-action">
               <?= $relatedPage->title() ?>
@@ -45,10 +51,6 @@
 
     </div>
   <?php endforeach; ?>
-</div>
-
-
-
 </div>
 
 
