070b272b9d15cca7bb9ddfbd96c852109318676d
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-26 12:15:23 +0100

    Die erste Seite nun nun die neue Standardseite default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,5 +4,25 @@
 
 <?php snippet('sidebar') ?>
 
+<?php if ($page->blogtag()->isNotEmpty()): ?>
+
+    <div class="container mt-4">
+
+        <h2 class="text-2xl border-t">
+            Blogeinträge für das Tag
+        </h2>
+
+        <?php
+        foreach (page('blogs')->children()->listed()->filterBy('tags', $page->blogtag(), ',')->flip() as $subpage):
+
+            snippet('blogkarte', ['subpage' => $subpage]);
+            ?>
+
+        <?php endforeach ?>
+
+    </div>
+<?php endif ?>
+
+
 
 <?php snippet('footertw') ?>
