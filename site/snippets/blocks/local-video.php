5b194b2d7a74ba0da6183aa77430d749a848faf4
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-29 16:55:43 +0100

    TailwindCSS++ lokalvideo

--- site/snippets/blocks/local-video.php
+++ site/snippets/blocks/local-video.php
@@ -1,21 +1,15 @@
-<?php if ($block->video()->isNotEmpty()) : ?>
-  <div class="col col-md-6">
+<?php if ($block->video()->isNotEmpty()): ?>
+  <video id="video" class="w-1/2" src="<?= $block->video()->toFile() ?>"></video>
 
-    <video id="video" class="w-100" controls="" src="<?= $block->video()->toFile() ?>"></video>
-    
-    <?php if ($block->name()->isNotEmpty() || $block->autor()->isNotEmpty()) : ?>
-      <div class="blockquote-footer pt-3">
+  <?php if ($block->name()->isNotEmpty() || $block->autor()->isNotEmpty()): ?>
+    <div class="w-full">
+      <p class="text-2xl font-bold text-center">
+        <?= $block->name() ?>
+      </p>
+      <p class="text-xl font-bold text-center">
+        <?= $block->autor() ?>
+      </p>
+    </div>
+  <?php endif ?>
 
-        <?php if ($block->name()->isNotEmpty()) : ?>          
-            <cite class="me-3" title="Source Title"><?= $block->name() ?></cite>          
-        <?php endif ?>
-
-        <?php if ($block->autor()->isNotEmpty()) : ?>          
-            <cite title="Source Author">von: <?= $block->autor() ?></cite>          
-        <?php endif ?>
-        
-      </div>
-    <?php endif ?>
-
-  </div>
 <?php endif ?>
