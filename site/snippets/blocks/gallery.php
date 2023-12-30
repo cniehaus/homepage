1ed76c4867954c2a15baddba34a10a7ff373e3db
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-30 12:04:24 +0100

    Vernünftiges TailwindCSS

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -1,33 +1,30 @@
 <?php
 
-/** @var \Kirby\Cms\Block $block */ ?>
+/** @var \Kirby\Cms\Block $block */?>
 
-
-
-
-
-<!-- Image Overlays Section: Image Zoom In -->
+<!-- https://getkirby.com/docs/reference/panel/blocks/gallery -->
 <div class="bg-white dark:bg-gray-900 dark:text-gray-100">
     <div class="container mx-auto space-y-16 px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
         <div class="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
 
-            <?php foreach ($block->images()->toFiles() as $image) : ?>
+            <?php foreach ($block->images()->toFiles() as $image): ?>
 
-                <div class="group relative overflow-hidden rounded-lg bg-indigo-900 focus-within:ring-4 focus-within:ring-indigo-500 focus-within:ring-opacity-50 focus-within:ring-offset-2 focus:outline-none focus:ring-4 focus:ring-indigo-500 focus:ring-opacity-50 focus:ring-offset-2 dark:ring-offset-gray-900" tabindex="0">
-                    <img src="<?= $image->url() ?>" class="transition duration-300 ease-out group-focus-within:scale-125 group-hover:scale-125 group-focus:scale-125" alt="Image 1" />
+                <div class="group relative overflow-hidden rounded-lg focus-within:ring-4 focus-within:ring-indigo-500 focus-within:ring-opacity-50 focus-within:ring-offset-2 focus:outline-none focus:ring-4 focus:ring-indigo-500 focus:ring-opacity-50 focus:ring-offset-2 dark:ring-offset-gray-900"
+                    tabindex="0">
+                    <img src="<?= $image->url() ?>"
+                        class="transition duration-300 ease-out group-focus-within:scale-125 group-hover:scale-125 group-focus:scale-125"
+                        alt="Image 1" />
 
-                    <!-- Item Overlay -->
-                    <div class="absolute inset-0 flex flex-col items-center justify-center rounded bg-slate-900 bg-opacity-80 opacity-0 transition duration-300 ease-out group-focus-within:opacity-100 group-hover:opacity-100 group-focus:opacity-100">
-                        <div class="text-center">
-                            <h4 class="text-lg font-semibold text-white"><?= $image->beschreibung() ?></h4>
-                        </div>
-                    </div>
-                    <!-- END Item Overlay -->
+                    <p>
+                        <?= $image->beschreibung() ?>
+                    </p>
                 </div>
 
             <?php endforeach ?>
 
         </div>
+        <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">
+            <?= $block->caption() ?>
+        </h2>
     </div>
 </div>
-<!-- END Image Overlays Section: Image Zoom In -->
