5b8448090a43b4eb48f213730d65699eb6f1fdd2
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-30 15:28:44 +0100

    TailwindCSS von tailwindui übernommen

--- site/snippets/blocks/tabelle.php
+++ site/snippets/blocks/tabelle.php
@@ -5,63 +5,72 @@ $empty_head = true;
 $empty_body = true;
 
 //Zuerst muss überprüft werden, ob die Tabellen leer sind. Da die Tabellen, aber aus einem array in einem anderen array bestehen ist das etwas umständlicher
-if(isset($table_head)) : //gibt es eine Tabelle?
+if (isset($table_head)): //gibt es eine Tabelle?
   foreach ($table_head as $tableRow): //Es wird überprüft ob die Tabellen-Überschirft komplett leer ist
-    foreach ($tableRow as $tableCell) :
-      if(!empty($tableCell))
+    foreach ($tableRow as $tableCell):
+      if (!empty($tableCell))
         $empty_head = false; //Falls irgendwo Inhalt gefunden wird, wird die Variable auf falsch gesetzt
     endforeach;
   endforeach;
 endif;
 
-if(isset($table_body)) : //gibt es eine Tabelle?
+if (isset($table_body)): //gibt es eine Tabelle?
   foreach ($table_body as $tableRow): //Es wird überprüft ob die Tabellen-Inhalt komplett leer ist
-    foreach ($tableRow as $tableCell) :
-      if(!empty($tableCell))
+    foreach ($tableRow as $tableCell):
+      if (!empty($tableCell))
         $empty_body = false; //Falls irgendwo Inhalt gefunden wird, wird die Variable auf falsch gesetzt
     endforeach;
   endforeach;
 endif; ?>
 
 
-<?php if(!$empty_head) : //Wenn der Tabellen-Kopf nicht leer ist wird die Tabelle angelegt und die Überschrift eingefügt?>
-    <div class="table-responsive">
-      <table class="table">
-        <thead>
-        <?php foreach ($table_head as $tableRow): ?>
-          <tr>
-            <?php foreach ($tableRow as $tableCell): ?>
-              <th><?= $tableCell ?></th>
+<?php if (!$empty_head): //Wenn der Tabellen-Kopf nicht leer ist wird die Tabelle angelegt und die Überschrift eingefügt?>
+  <div class="mt-8 flow-root">
+    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <table class="min-w-full divide-y divide-gray-300">
+          <thead>
+            <?php foreach ($table_head as $tableRow): ?>
+              <tr>
+                <?php foreach ($tableRow as $tableCell): ?>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <?= $tableCell ?>
+                  </th>
+                <?php endforeach ?>
+              </tr>
             <?php endforeach ?>
-          </tr>
-        <?php endforeach ?>
-        </thead>
+          </thead>
 
-  <?php if($empty_body) : //Wenn danach kein Tabellen-Inhalt kommt -> Tabelle beenden ?>
-      </table>
-    </div>
-  <?php endif ?>
-  
-<?php endif ?>
+          <?php if ($empty_body): //Wenn danach kein Tabellen-Inhalt kommt -> Tabelle beenden ?>
+          </table>
+        <?php endif ?>
 
-<?php if(!$empty_body) : //Wenn Tabellen-Inhalt nicht leer ist wird der Inhalt eingefügt ?>
+      <?php endif ?>
 
-  <?php if($empty_head) : //Wenn davor kein Tabellen-Kopf war -> neue Tabelle starten ?>
-    <div class="table-responsive">
-      <table class="table">
-  <?php endif ?>
+      <?php if (!$empty_body): //Wenn Tabellen-Inhalt nicht leer ist wird der Inhalt eingefügt ?>
 
-        <tbody>
-        <?php foreach ($table_body as $tableRow): ?>
-          <tr>
-            <?php foreach ($tableRow as $tableCell): ?>
-              <td><?= $tableCell ?></td>
-            <?php endforeach ?>
-          </tr>
-        <?php endforeach ?>
-        </tbody>
+        <?php if ($empty_head): //Wenn davor kein Tabellen-Kopf war -> neue Tabelle starten ?>
+          <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table class=" min-w-full divide-y divide-gray-300">
+                <?php endif ?>
 
-      </table>
-    </div>
+                <tbody class="divide-y divide-gray-200">
+                  <?php foreach ($table_body as $tableRow): ?>
+                    <tr>
+                      <?php foreach ($tableRow as $tableCell): ?>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <?= $tableCell ?>
+                        </td>
+                      <?php endforeach ?>
+                    </tr>
+                  <?php endforeach ?>
+                </tbody>
 
-<?php endif ?>
+              </table>
+
+            <?php endif ?>
+          </div>
+        </div>
+      </div>
