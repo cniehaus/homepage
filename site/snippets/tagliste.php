488a1295bba353194283c65774356c3695813965
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-01 15:02:30 +0100

    tags nun auch portiert, 4 weitere Klassen Bootstrap weg

--- site/snippets/tagliste.php
+++ site/snippets/tagliste.php
@@ -1,14 +1,14 @@
-<?php if ($item->tags()->isNotEmpty()) : ?>
-    <span>
-        <i class="bi bi-tags"></i> Tags:
+<?php if ($item->tags()->isNotEmpty()): ?>
+    <i class="bi bi-tags"></i>
 
-        <?php foreach ($item->tags()->split() as $tag) : ?>
-            <a  class="badge rounded-pill bg-info text-decoration-none"
-                href="<?= url('blogs', ['params' => ['tag' => urlencode($tag)]]) ?>">
-                
+    <?php foreach ($item->tags()->split() as $tag): ?>
+        <div
+            class="ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-kgsblue-light text-white rounded-full">
+
+            <a href="<?= url('blogs', ['params' => ['tag' => urlencode($tag)]]) ?>">
+
                 <?= $tag ?>
             </a>
-        <?php endforeach ?>
-
-    </span>
+        </div>
+    <?php endforeach ?>
 <?php endif ?>
