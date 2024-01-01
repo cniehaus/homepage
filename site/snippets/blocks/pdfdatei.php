46eab5ff543ba263295ecab73bdfd446a9eb8637
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-01 14:17:20 +0100

    das war der letzte btn (Button) von Bootstrap, entfernt eine Menge Code

--- site/snippets/blocks/pdfdatei.php
+++ site/snippets/blocks/pdfdatei.php
@@ -3,8 +3,10 @@
  -->
 <div class="container">
   <a href="<?= $block->pdfdatei()->toFile() ?>">
-    <button type="button" class="btn btn-lg bd-btn-lg btn-bd-danger d-flex align-items-center justify-content-center fw-semibold">
-      <i class="bi bi-file-pdf"></i> <?= $block->beschriftung() ?>
+    <button type="button"
+      class="px-4 py-1 text-gray-800 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-slate-400 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 text-center"
+      <i class="bi bi-file-pdf"></i>
+      <?= $block->beschriftung() ?>
     </button>
   </a>
 </div>
