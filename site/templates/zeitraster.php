6e9130294ab932277e06562ec8d01eba225b954d
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-01 13:45:00 +0100

    Zeitraster nun auch TailwindCSS, löscht 4 Bootstrapklassen im CSS

--- site/templates/zeitraster.php
+++ site/templates/zeitraster.php
@@ -1,57 +1,66 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-
 <?php snippet('sidebar') ?>
 
-<div class="container">
+<div class="container mx-auto">
 
-    <table class="table table-borderless table-responsive table-striped">
-        <!-- https://getbootstrap.com/docs/5.1/content/tables/ -->
-        <tr>
-            <th class="text-center">Stunde</th>
-            <th>von</th>
-            <th>bis</th>
-        </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="text-center">1</td>
-                <td>08.00</td>
-                <td>08.45</td>
-            </tr>
-            <tr>
-                <td class="text-center">2</td>
-                <td>08.55</td>
-                <td>09.40</td>
-            </tr>
-            <tr>
-                <td class="text-center">3/4</td>
-                <td>10.00</td>
-                <td>11.30</td>
-            </tr>
-            <tr>
-                <td class="text-center">5</td>
-                <td>11.50</td>
-                <td>12.35</td>
-            </tr>
-            <tr>
-                <td class="text-center">6</td>
-                <td>12.45</td>
-                <td>13.30</td>
-            </tr>
-            <tr>
-                <td class="text-center">Mittagspause</td>
-                <td>13.30</td>
-                <td>14.30</td>
-            </tr>
-            <tr>
-                <td class="text-center">7/8</td>
-                <td>14.30</td>
-                <td>16.00*</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="my-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table class="min-w-full divide-y divide-gray-300">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="text-center px-3 py-3.5 font-semibold text-gray-900">Stunde
+                            </th>
+
+                            <th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">von</th>
+                            <th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">bis</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">1</td>
+                            <td>08.00</td>
+                            <td>08.45</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">2</td>
+                            <td>08.55</td>
+                            <td>09.40</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">3/4</td>
+                            <td>10.00</td>
+                            <td>11.30</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">5</td>
+                            <td>11.50</td>
+                            <td>12.35</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">6</td>
+                            <td>12.45</td>
+                            <td>13.30</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">Mittagspause</td>
+                            <td>13.30</td>
+                            <td>14.30</td>
+                        </tr>
+                        <tr class="even:bg-gray-50 odd:bg-slate-300">
+                            <td class="text-center">7/8</td>
+                            <td>14.30</td>
+                            <td>16.00*</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <?php snippet('footertw') ?>
