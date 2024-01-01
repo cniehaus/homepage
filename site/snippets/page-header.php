98037b227ce98c87e490fda1165b03399ab5d769
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-01 15:11:29 +0100

    hier versteckte sich noch Bootstrap

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,6 +1,6 @@
 <main role="main">
   <?php
-  if ($image = $page->hintergrundbild()->toFile()) : ?>
+  if ($image = $page->hintergrundbild()->toFile()): ?>
 
     <div class="p-5 text-center bg-image" style="
       background-image: url('<?= $image->url() ?>');
@@ -8,28 +8,26 @@
       background-size: cover;
       height:30vh">
 
-  <?php else : ?>
-    <div class="p-5 text-center bg-image" style="
+    <?php else: ?>
+      <div class="p-5 text-center bg-image" style="
       background-image: url('<?= $kirby->url('assets') ?>/img/IMG_1343.jpeg');
       background-repeat: no-repeat;
       background-size: cover;
       height:30vh">
-  <?php endif ?>
+      <?php endif ?>
 
     </div>
 
 
 
 
-  <!-- Titelfeld -->
+    <!-- Titelfeld -->
+    <div class="bg-light text-white p-3 mb-2">
+      <h1 class="text-2xl">
+        <?= $page->title() ?>
+      </h1>
+    </div>
 
-  <div class="p-3 mb-2 bg-light text-secondary">
-    <h1>
-      <?= $page->title() ?>
+    <h1 class="text-xl mb-3 ms-3">
+      <?= $page->heading() ?>
     </h1>
-  </div>
-  <small class="text-muted"><?= $page->subtitle() ?></small>
-
-  <h1 class="display-6 mb-3 ms-3">
-    <?= $page->heading() ?>
-  </h1>
