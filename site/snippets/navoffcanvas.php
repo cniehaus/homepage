7a0ed50a7da659d7010d59867b59471008e3c371
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-11 15:57:02 +0100

    Die Suche ersetzt. Eine Menge Bootstrap fällt damit flach

--- site/snippets/navoffcanvas.php
+++ site/snippets/navoffcanvas.php
@@ -1,9 +1,9 @@
 <?php $kategorien = page('navbar')->navbar()->toStructure() ?>
 <?php $count = 1 //Wird für die id der Kategorien verwendet, damit diese einmalig bleiben 
-?>
+    ?>
 
-<?php if ($kategorien->isNotEmpty()) : //Erst die Grundstruktur für die nav 
-?>
+<?php if ($kategorien->isNotEmpty()): //Erst die Grundstruktur für die nav 
+        ?>
 
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-secondary" aria-label="Main navigation">
@@ -21,13 +21,17 @@
             <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <?php //Jetzt kommen alle Kategorien
-                    snippet('navkategorien', ['items' => $kategorien, 'count' => $count]) ?>
+                        snippet('navkategorien', ['items' => $kategorien, 'count' => $count]) ?>
                 </ul>
-                <form class="d-flex" action="/search">
-                    <input class="form-control me-2" type="text" placeholder="Suche" name="q" <button type="submit"></button>
+
+                <!-- Search -->
+                <form action="/search">
+                    <input type="text"
+                        class="block w-full rounded-lg border border-gray-200 py-2 text-sm leading-5 placeholder-gray-400 focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 dark:border-gray-700 dark:bg-gray-900/25 dark:focus:border-indigo-500"
+                        id="search" name="q" placeholder="Suche..." />
                 </form>
 
-
+                <!-- END Search -->
             </div>
         </div>
     </nav>
