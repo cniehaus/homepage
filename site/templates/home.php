d2b31d9832f99731146898ee705e1418389e74db
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-12-10 20:00:12 +0100

    Notefallseite

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,25 +1,35 @@
 <?php snippet('header') ?>
 <?php snippet('home-page-header') ?>
 
-<?php snippet('box-notfall') ?>
 
-<div class="mx-4 flex flex-wrap">
-  <!-- Major Column (2/3 width) -->
-  <div class="lg:w-2/3 mb-8 lg:mb-0 pr-4"> <!-- Added pr-4 for right margin -->
-    <?php snippet('box-fokus') ?>
-    <?php snippet('box-blogs') ?>
+<?php if ($page->togglenotfall()->toggle()->bool() === false) : ?>
+  <div class="rounded-lg border-8 border-dashed border-gray-800">
+    <article class="bg-red-400 py-8 px-8 text-2xl leading-10 prose-h1:text-lg dark:bg-gray-700/25 prose-a:text-blue-600">
+      <?= $page->notfalltext()->kirbytext() ?>
+    </article>
   </div>
 
-  <!-- Sidebar Column (1/3 width) -->
-  <div class="lg:w-1/3">
-    <?php snippet('box-kalender') ?>
-    <?php snippet('box-presse') ?>
+<?php else: ?>
+  <?php snippet('box-notfall') ?>
+
+  <div class="mx-4 flex flex-wrap">
+    <!-- Major Column (2/3 width) -->
+    <div class="lg:w-2/3 mb-8 lg:mb-0 pr-4"> <!-- Added pr-4 for right margin -->
+      <?php snippet('box-fokus') ?>
+      <?php snippet('box-blogs') ?>
+    </div>
+
+    <!-- Sidebar Column (1/3 width) -->
+    <div class="lg:w-1/3">
+      <?php snippet('box-kalender') ?>
+      <?php snippet('box-presse') ?>
+    </div>
   </div>
-</div>
 
-<?php snippet('box-links') ?>
+  <?php snippet('box-links') ?>
 
 
+<?php endif ?>
 
 
 <?php snippet('footertw') ?>
