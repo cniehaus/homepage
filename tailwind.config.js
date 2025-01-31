9b66fc36fa684b96ebb03068f7ec0ac353d75f7a
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-01-31 09:07:21 +0100

    weniger ist mehr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        kgsblue: {
-          dark: "#0e5499",
-          light: "#8e9fcc",
-        },
-        kgsred: "#a34e1c",
-      },
-    },
-  },
-  variants: {},
   plugins: [require("@tailwindcss/typography")],
 };
