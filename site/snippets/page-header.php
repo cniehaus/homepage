e91e73acf2bebc8910ab8617e750a13e7c78be29
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-02-03 16:14:57 +0100

    hero bereich in beide arten von page header

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,33 +1,36 @@
 <main role="main">
-  <?php
-  if ($image = $page->hintergrundbild()->toFile()): ?>
 
-    <div class="p-5 text-center bg-image" style="
-      background-image: url('<?= $image->url() ?>');
-      background-repeat: no-repeat;
-      background-size: cover;
-      height:30vh">
+  <!--
 
-    <?php else: ?>
-      <div class="p-5 text-center bg-image" style="
-      background-image: url('<?= $kirby->url('assets') ?>/img/IMG_1343.jpeg');
-      background-repeat: no-repeat;
-      background-size: cover;
-      height:30vh">
-      <?php endif ?>
+  ADD YOUR HERO CONTENT BELOW
 
-    </div>
+  -->
 
+  <!-- Placeholder -->
+  <div class="container mx-auto px-4 pb-12 lg:px-8 xl:max-w-7xl">
+    <div
+      class="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-64 text-gray-400 dark:border-gray-700/75 dark:bg-gray-800/50">
 
+      <!-- Titelfeld -->
+      <div class="bg-light text-black p-3 mb-2">
+        <h1 class="text-2xl">
+          <?= $page->title() ?>
+        </h1>
+      </div>
 
-
-    <!-- Titelfeld -->
-    <div class="bg-light text-white p-3 mb-2">
-      <h1 class="text-2xl">
-        <?= $page->title() ?>
+      <h1 class="text-xl mb-3 ms-3">
+        <?= $page->heading() ?>
       </h1>
-    </div>
 
-    <h1 class="text-xl mb-3 ms-3">
-      <?= $page->heading() ?>
-    </h1>
+    </div>
+  </div>
+
+  <!--
+  
+  ADD YOUR HERO CONTENT ABOVE
+  
+  -->
+
+
+
+  </div><!-- dieser DIV beended den ersten DIV vom Megamenü -->
