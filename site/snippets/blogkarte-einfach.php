39e45bd595211553b9115887766d1186b0d781df
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-02-04 17:15:24 +0100

    Blogs neu mit Tailkit 4.2, etwas kontrastarm

--- site/snippets/blogkarte-einfach.php
+++ site/snippets/blogkarte-einfach.php
@@ -1,19 +1,19 @@
-<div
-    class="flex flex-col shadow-xl ring-1 ring-gray-900/5 rounded-lg sm:max-w-lg sm:px-2 md:px-4 h-full bg-slate-50 hover:bg-slate-100">
-
-    <p class="text-lg font-bold mt-auto text-red-700">
-
-        <?= $subpage->title() ?>
-    </p>
-
-    <?= $subpage->Text()->toBlocks()->excerpt(250) ?>
-    <?php if ($subpage->author()->isNotEmpty()): ?>
-        (
-        <?= $subpage->author() ?>)
-    <?php endif ?>
-    </p>
-
-    <div class="text-right mt-2 mb-2">
-        <?php snippet('knopf-klein', ['subpage' => $subpage, 'knopftext' => "weiterlesen"]); ?>
+<a href="<?= $subpage->url() ?>" class="group relative block">
+    <div
+        class="absolute inset-0 scale-0 rounded-sm bg-gray-100 opacity-0 transition group-hover:scale-110 group-hover:opacity-100 group-active:bg-indigo-100 dark:bg-gray-800 dark:group-active:bg-indigo-600/25"></div>
+    <div class="relative">
+        <p
+            class="mt-3 mb-1 text-sm font-medium text-gray-600 dark:text-red-400">
+            <span class="font-medium"><?= $subpage->date()->toDate("d.m.Y") ?></span>
+        </p>
+        <h4
+            class="mb-2 text-lg leading-6 font-bold text-gray-800 dark:text-gray-200">
+            <?= $subpage->title() ?>
+        </h4>
+        <p
+            class="mt-3 mb-1 text-sm font-medium text-gray-600 dark:text-gray-400">
+            <span class="font-medium"> <?= $subpage->Text()->toBlocks()->excerpt(250) ?>
+            </span>
+        </p>
     </div>
-</div>
+</a>
--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -2,18 +2,24 @@
 
 <?php snippet('page-header') ?>
 
+<!-- Blog List Section: In Grid Alternate -->
+<div class="bg-white dark:bg-gray-900 dark:text-gray-100">
+  <div
+    class="container mx-auto space-y-16 px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
 
-
-<div class="bg-slate-50 dark:bg-gray-900 dark:text-gray-100 px-4 pb-6 mb-8">
-  <!-- Latest Posts -->
-  <div class="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-    <?php foreach ($articles as $article)
-      snippet('blogkarte-einfach', [
-        'subpage' => $article
-      ])
-    ?>
+    <!-- Latest Posts -->
+    <div class="grid grid-cols-1 gap-8 md:grid-cols-3 lg:gap-10">
+      <?php foreach ($articles as $article)
+        snippet('blogkarte-einfach', [
+          'subpage' => $article
+        ])
+      ?>
+    </div>
+    <!-- END Latest Posts -->
   </div>
 </div>
+<!-- END Blog List Section: In Grid Alternate -->
+
 
 
 <div class="d-flex justify-content-center">
