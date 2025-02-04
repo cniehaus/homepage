496ce9db7112db79f546c6ed42e0ae4e4f10feac
Ole Bohmbach <mail@olebohmbach.eu>
2025-02-04 23:12:19 +0100

    fix(snippet): blogs without any tags are now working

--- site/snippets/tagliste.php
+++ site/snippets/tagliste.php
@@ -3,14 +3,10 @@
         <?php foreach ($item->tags()->split() as $tag): ?>
             <div
                 class="inline-flex rounded-full bg-indigo-100 px-2 py-1 text-sm leading-4 font-semibold text-indigo-800 dark:bg-indigo-900/75 dark:text-indigo-200">
-                <a href="<?= url('blogs', [
-                  'params' => ['tag' => urlencode($tag)],
-                ]) ?>">
-                    <?= $tag ?>
+                <a href="<?= url('blogs', ['params' => ['tag' => urlencode($tag)]]) ?>">
+                    <?= htmlspecialchars($tag, ENT_QUOTES, 'UTF-8') ?>
                 </a>
             </div>
-        <?php endforeach; ?>
+        <?php endforeach ?>
     </div>
-<?php endif; ?>
-
-<a href="<?= url('blogs', ['params' => ['tag' => urlencode($tag)]]) ?>">
+<?php endif ?>
