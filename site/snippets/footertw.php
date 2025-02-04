884765bdd90e34a5dfd79e288ecb91e262787205
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-02-04 10:46:48 +0100

    Unser Instagram Account nun auch verlinkt

--- site/snippets/footertw.php
+++ site/snippets/footertw.php
@@ -63,16 +63,33 @@
             </nav>
         </div>
 
-        <!--Copyright section-->
-        <div class="bg-neutral-200 py-2 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+    </div>
+    <hr class="my-10 border-dashed border-gray-200 dark:border-gray-700/75" />
+    <div
+        class="flex flex-col gap-6 text-center text-sm md:flex-row-reverse md:justify-between md:gap-0 md:text-left">
+        <nav class="space-x-4">
 
-            <div class="col" id="copyrightText">
-            </div>
+            <a href="https://www.instagram.com/kgsrastede_offiziell/" class="text-gray-400 hover:text-[#405de6]">
+                <svg
+                    class="icon-instagram inline-block size-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor">
+                    <path
+                        d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"></path>
+                </svg>
+            </a>
 
-            <script>
-                document.getElementById("copyrightText").innerHTML = `Copyright &#xA9; ${new Date().getFullYear()} KGS Rastede ; All Rights Reserved.`;
-            </script>
 
+        </nav>
+        <div class="text-gray-500 dark:text-gray-400/80">
+            <span class="font-medium">
+                <div class="col" id="copyrightText">
+                    <script>
+                        document.getElementById("copyrightText").innerHTML = `Copyright &#xA9; ${new Date().getFullYear()} KGS Rastede ; All Rights Reserved.`;
+                    </script>
+                </div>
+            </span>
         </div>
     </div>
 </footer>
