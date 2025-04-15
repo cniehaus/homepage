005cecb25eaad7b08a1fa41e2bf1f69a7ca24b65
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-04-15 18:28:38 +0200

    Der ganze Knopf soll ein Link sein!

--- site/templates/schulbuchlisten.php
+++ site/templates/schulbuchlisten.php
@@ -45,13 +45,15 @@ slot();
   <div class="flex max-w-xl flex-col items-start justify-between">
     <div class="group relative">
       <?php foreach ($page->SchulbuchlistenRZ()->toStructure() as $liste): ?>
-        <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
-          <div class="flex justify-between">
-            <dt>
-              <?= $liste->name() ?>
-            </dt>
-            <dd class="text-gray-900">
-              <a href="<?= $liste->link()->toFile()->url() ?>">
+        <a href="<?= $liste->link()->toFile()->url() ?>">
+
+          <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+
+            <div class="flex justify-between">
+              <dt>
+                <?= $liste->name() ?>
+              </dt>
+              <dd class="text-gray-900">
                 <button type="button"
                   class="ml-5 rounded-full bg-slate-100 p-1.5 text-black shadow-sm hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-900">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -63,10 +65,10 @@ slot();
                       d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
                   </svg>
                 </button>
-              </a>
-            </dd>
-          </div>
-        </dl>
+              </dd>
+            </div>
+          </dl>
+        </a>
       <?php endforeach; ?>
     </div>
   </div>
