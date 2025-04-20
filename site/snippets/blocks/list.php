3e277c31b5a445678adb1bda166f24b270bd4b95
Carsten Niehaus <ni@kgs-rastede.eu>
2025-04-20 22:00:27 +0200

    auf großen Bildschirmen wie im Haupttext die Schrift anpassen

--- site/snippets/blocks/list.php
+++ site/snippets/blocks/list.php
@@ -2,7 +2,7 @@
 /** @var \Kirby\Cms\Block $block */
 ?>
 
-<article class="prose-li:list-disc pl-16">
+<article class="prose-li:list-disc pl-16 lg:prose-xl prose-a:text-blue-600">
 
     <?= $block->text() ?>
 
