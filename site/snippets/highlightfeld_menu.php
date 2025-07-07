0795402d5df6f9ae88bc118983794ef559e08a2e
Carsten Niehaus <ni@kgs-rastede.eu>
2025-07-07 14:21:51 +0200

    Bilder auswählbar

--- site/snippets/highlightfeld_menu.php
+++ site/snippets/highlightfeld_menu.php
@@ -4,7 +4,7 @@
     class="block space-y-3 text-sm font-semibold transition-opacity hover:text-kgs-blue hover:opacity-90 dark:hover:text-kgs-lblue">
 
     <img
-      src="https://cdn.tailkit.com/media/placeholders/photo-8pb7Hq539Zw-300x180.jpg"
+      src="<?= $feld_bild->url() ?>"
       class="rounded-lg"
       alt="Preview Image" />
     <h5>
